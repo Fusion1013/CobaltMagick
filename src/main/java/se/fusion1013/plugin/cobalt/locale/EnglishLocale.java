a63eda132023c2a98bf3d9671d043b8b2ab6625c
Fusion1013 <theminecubes1@hotmail.com>
2021-09-27 14:37:36 +0200

    Add translations for scenario commands Generalize translations

--- src/main/java/se/fusion1013/plugin/cobalt/locale/EnglishLocale.java
+++ src/main/java/se/fusion1013/plugin/cobalt/locale/EnglishLocale.java
@@ -34,17 +34,27 @@ public class EnglishLocale implements Locale {
                 this.put("#2", "Emitter Command Success Messages");
                 this.put("command-success-emitter-create", "&eCreated new emitter. &eName: &b%name% &eLocation: &b%x%&ex &b%y%&ey &b%z%&ez");
 
-                this.put("#3", "Event Command Usage Messages");
-                this.put("command-usage-event-create", "/scenario create <type> [data]");
-                this.put("command-usage-event-start", "/scenario play <event>");
+                this.put("#3", "Scenario Command Usage Messages");
+                this.put("command-usage-scenario-create", "/scenario create <name> [data]");
+                this.put("command-usage-scenario-start", "/scenario start <event>");
 
                 this.put("#10", "Misc");
                 this.put("command-not-implemented", "&eCommand not yet implemented");
                 this.put("command-unknown", "&eUnknown command");
 
                 this.put("#11", "List Messages");
+                this.put("list-header", "<g:#00aaaa:#0066aa>------ %header% ------");
+                this.put("list-item-name", "&eName: &b%name%");
+                this.put("list-item-location", "&eLocation: &b%x%&ex &b%y%&ey &b%z%&ez &eWorld: &b%world%");
+                this.put("list-item-id-name-location", "&e[&b%id%&e] &eName: &b%name% &eLocation: &b%x%&ex &b%y%&ey &b%z%&ez");
+
+                // DEPRECATED --
                 this.put("list-emitter-groups", "<g:#00aaaa:#0066aa>------ Available Emitters ------");
-                this.put("list-emitter-group-output", "&eName: &b%name% &e Location: &b%x%&ex &b%y%&ey &b%z%&ez");
+                this.put("list-emitter-group-output", "&e[&b%id%&e] &eName: &b%name% &eLocation: &b%x%&ex &b%y%&ey &b%z%&ez");
+                this.put("list-scenarios", "<g:#00aaaa:#0066aa>------ Available Scenarios ------");
+                this.put("list-scenarios-output", "&e[&b%id%&e] &eName: &b%name% &eLocation: &b%x%&ex &b%y%&ey &b%z%&ez &eWorld: &b%world%");
+                // --DEPRECATED
+
 
                 this.put("gradient", "<g:#ff1100:#00ff1e>GRADIENT");
             }
