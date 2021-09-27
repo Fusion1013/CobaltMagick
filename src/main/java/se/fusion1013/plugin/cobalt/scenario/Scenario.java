d2ff5484d63654b1d82d16bcf140a5df1881f7cd
Fusion1013 <theminecubes1@hotmail.com>
2021-09-27 14:39:30 +0200

    Add scenario methods

--- src/main/java/se/fusion1013/plugin/cobalt/scenario/Scenario.java
+++ src/main/java/se/fusion1013/plugin/cobalt/scenario/Scenario.java
@@ -1,20 +1,65 @@
 package se.fusion1013.plugin.cobalt.scenario;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.command.CommandSender;
+import se.fusion1013.plugin.cobalt.Cobalt;
+import se.fusion1013.plugin.cobalt.manager.CustomEventManager;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Scenario {
     private List<CustomEvent> events;
 
-    public Scenario(){
-        events = new ArrayList<>();
+    private String name;
+    private Location scenarioLocation;
+
+    public Scenario(String name, Location scenarioLocation){
+        this.name = name;
+        this.scenarioLocation = scenarioLocation;
     }
 
-    public void addEvent(String localEventName){
+    public Scenario(String name, World world){
+        this(name, new Location(world, 0, 0, 0));
+    }
 
+    //TODO: Event system
+    public void addEvent(String eventName){
+        addEvent(Cobalt.getInstance().getManager(CustomEventManager.class).getEventByName(eventName));
     }
 
     private void addEvent(CustomEvent event){
         events.add(event);
+    }
+
+    public void printScenarioInfo(CommandSender sender){
+
+    }
+
+    /***
+     * Returns the name of the scenario
+     * @return scenario name
+     */
+    public String getName(){
+        return this.name;
+    }
+
+    /***
+     * Returns the location of the scenario
+     * @return scenario location
+     */
+    public Location getScenarioLocation(){
+        return this.scenarioLocation;
+    }
+
+    /***
+     * Sets the scenario locaiton
+     * @param scenarioLocation scenario location
+     */
+    public Scenario setScenarioLocation(Location scenarioLocation){
+        this.scenarioLocation = scenarioLocation;
+        return this;
     }
 }
