777867abfe76aff7504e2714453f675737fa52e2
Fusion1013 <william.reutmer@gmail.com>
2021-11-19 08:12:26 +0100

    update WarpCommand to list warps on default use

--- src/main/java/se/fusion1013/plugin/cobalt/commands/WarpCommand.java
+++ src/main/java/se/fusion1013/plugin/cobalt/commands/WarpCommand.java
@@ -2,19 +2,19 @@ package se.fusion1013.plugin.cobalt.commands;
 
 import org.bukkit.command.CommandSender;
 
-import java.util.List;
-
 @CommandDeclaration(
         commandName = "warp",
         aliases = "w",
         permission = "cobalt.warp",
         usage = "/warp",
         description = "Warp",
-        validSenders = SenderType.PLAYER
+        validSenders = SenderType.PLAYER,
+        maxArgs = 0,
+        executeIfInvalidSubCommand = true
 )
 public class WarpCommand extends MainCommand {
     @Override
     public boolean execute(CommandSender sender, String[] args, CommandFlags flags) {
-        return true;
+        return getSubCommands().get("list").validate(sender, args);
     }
 }
