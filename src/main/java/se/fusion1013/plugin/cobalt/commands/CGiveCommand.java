293219f587f23f0ab56b91cfcb65a0dc8594507f
Fusion1013 <william.reutmer@gmail.com>
2021-11-22 19:36:45 +0100

    make give wand to player

--- src/main/java/se/fusion1013/plugin/cobalt/commands/CGiveCommand.java
+++ src/main/java/se/fusion1013/plugin/cobalt/commands/CGiveCommand.java
@@ -1,5 +1,6 @@
 package se.fusion1013.plugin.cobalt.commands;
 
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.command.CommandSender;
@@ -8,6 +9,9 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
 import se.fusion1013.plugin.cobalt.Cobalt;
+import se.fusion1013.plugin.cobalt.wand.Wand;
+
+import java.util.ArrayList;
 
 @CommandDeclaration(
         commandName = "cgive",
@@ -19,15 +23,26 @@ import se.fusion1013.plugin.cobalt.Cobalt;
 public class CGiveCommand extends MainCommand {
 
     // TODO: This should work for multiple items, it should not just give a wand
+    // (This is all temporary code used for testing, that's why it's shit)
 
     @Override
     public boolean execute(CommandSender sender, String[] args, CommandFlags flags) {
+        Wand wand = new Wand(false, 2, 0.1, 0.4, 200, 20, 4, 5.0, new ArrayList<>(), 1);
+        Cobalt.getInstance().getRDatabase().insertWand(wand);
+
         ItemStack is = new ItemStack(Material.STICK, 1);
         ItemMeta meta = is.getItemMeta();
 
+        // Set Persistent Data
         NamespacedKey namespacedKey = new NamespacedKey(Cobalt.getInstance(), "wand_id");
-
         meta.getPersistentDataContainer().set(namespacedKey, PersistentDataType.INTEGER, 1);
+
+        // TODO: Move item generation to the wand class
+
+        // Set Wand Lore & Name
+        meta.setDisplayName(ChatColor.WHITE + "" + ChatColor.BOLD + "Wand");
+        meta.setLore(wand.getLore());
+
         is.setItemMeta(meta);
 
         Player player = (Player)sender;
