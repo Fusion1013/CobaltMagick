b0b423c3df3cdd66e3893d3a0314c16ddb5b6bba
Fusion1013 <william.reutmer@gmail.com>
2021-11-22 19:38:00 +0100

    add getSpellFromID shell method

--- src/main/java/se/fusion1013/plugin/cobalt/spells/Spell.java
+++ src/main/java/se/fusion1013/plugin/cobalt/spells/Spell.java
@@ -2,4 +2,10 @@ package se.fusion1013.plugin.cobalt.spells;
 
 public abstract class Spell implements ISpell {
 
+    // TODO: Register spells and make them accessible from here
+
+    public static ISpell getSpellFromID(int id){
+        // TODO
+        return null;
+    }
 }
