c9486137cf4a49d2ea0b12c24295b84dd15616b1
Fusion1013 <william.reutmer@gmail.com>
2021-11-22 19:38:47 +0100

    add getters/setters and add getLore method

--- src/main/java/se/fusion1013/plugin/cobalt/wand/Wand.java
+++ src/main/java/se/fusion1013/plugin/cobalt/wand/Wand.java
@@ -1,5 +1,7 @@
 package se.fusion1013.plugin.cobalt.wand;
 
+import net.md_5.bungee.api.ChatColor;
+import se.fusion1013.plugin.cobalt.Cobalt;
 import se.fusion1013.plugin.cobalt.spells.ISpell;
 
 import java.util.ArrayList;
@@ -10,6 +12,7 @@ public class Wand {
     // Shown Properties
     boolean shuffle;
     int spellsPerCast;
+
     double castDelay;
     double rechargeTime;
     int manaMax;
@@ -23,7 +26,9 @@ public class Wand {
     double rechargeCooldown;
 
     // Hidden Properties
+    int id;
     int wandTier;
+    int wandId; // This should be unique to all wands
 
     // Stored Spells
     List<ISpell> spells = new ArrayList<>();
@@ -42,6 +47,8 @@ public class Wand {
 
         this.currentMana = manaMax;
     }
+
+    // TODO: IMPLEMENT A WAND CACHE. CALLING THE DATABASE EVERY TIME A WAND IS USED IS EXTREMELY EXPENSIVE
 
     /**
      * Cast the next spells in the wand
@@ -91,10 +98,77 @@ public class Wand {
         return true;
     }
 
+    public void setSpells(List<ISpell> spells){
+        this.spells = spells;
+    }
+
     public enum CastResult{
         SUCCESS,
         RECHARGE_TIME,
         CAST_DELAY,
         NO_MANA
+    }
+
+    public List<String> getLore(){
+        List<String> lore = new ArrayList<>();
+
+        if (shuffle) lore.add(ChatColor.WHITE + "" + ChatColor.BOLD + "Shuffle: " + ChatColor.BLUE + "Yes");
+        else lore.add(ChatColor.WHITE + "" + ChatColor.BOLD + "Shuffle: " + ChatColor.BLUE + "No");
+        lore.add(ChatColor.WHITE + "" + ChatColor.BOLD + "Spells/Cast: " + ChatColor.BLUE + spellsPerCast);
+        lore.add(ChatColor.WHITE + "" + ChatColor.BOLD + "Cast Delay: " + ChatColor.BLUE + castDelay);
+        lore.add(ChatColor.WHITE + "" + ChatColor.BOLD + "Recharge Time: " + ChatColor.BLUE + rechargeTime);
+        lore.add(ChatColor.WHITE + "" + ChatColor.BOLD + "Mana Max: " + ChatColor.BLUE + manaMax);
+        lore.add(ChatColor.WHITE + "" + ChatColor.BOLD + "Mana Charge Speed: " + ChatColor.BLUE + manaChargeSpeed);
+        lore.add(ChatColor.WHITE + "" + ChatColor.BOLD + "Capacity: " + ChatColor.BLUE + capacity);
+        lore.add(ChatColor.WHITE + "" + ChatColor.BOLD + "Spread: " + ChatColor.BLUE + spread);
+        // TODO: Add always casts
+
+        // TODO: Find a way to display the spells that are currently in the wand (Could probably use bundles in the future)
+
+        return lore;
+    }
+
+    public boolean isShuffle() {
+        return shuffle;
+    }
+
+    public int getSpellsPerCast() {
+        return spellsPerCast;
+    }
+
+    public double getCastDelay() {
+        return castDelay;
+    }
+
+    public double getRechargeTime() {
+        return rechargeTime;
+    }
+
+    public int getManaMax() {
+        return manaMax;
+    }
+
+    public int getManaChargeSpeed() {
+        return manaChargeSpeed;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public double getSpread() {
+        return spread;
+    }
+
+    public List<ISpell> getAlwaysCast() {
+        return alwaysCast;
+    }
+
+    public int getWandTier() {
+        return wandTier;
+    }
+
+    public int getWandId() {
+        return wandId;
     }
 }
