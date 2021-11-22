201644532cca87cd30394ef6464b32e0613ff8bd
Fusion1013 <william.reutmer@gmail.com>
2021-11-22 19:37:37 +0100

    create wand gui

--- src/main/java/se/fusion1013/plugin/cobalt/gui/WandGUI.java
+++ src/main/java/se/fusion1013/plugin/cobalt/gui/WandGUI.java
@@ -1,7 +1,37 @@
 package se.fusion1013.plugin.cobalt.gui;
 
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
 public class WandGUI extends AbstractGUI {
+
+    static int realInvSize;
+
     public WandGUI(int invSize, String invName) {
-        super(invSize, invName);
+        super(getInventorySize(invSize), invName);
+        realInvSize = getInventorySize(invSize);
+
+        setInaccessibleSlots(realInvSize-invSize);
+    }
+
+    private void setInaccessibleSlots(int count){
+        ItemStack stack = new ItemStack(Material.WHITE_STAINED_GLASS_PANE);
+        ItemMeta meta = stack.getItemMeta();
+        meta.setDisplayName("--");
+        stack.setItemMeta(meta);
+
+        for (int i = 1; i <= count; i++){
+            setItem(realInvSize - i, stack);
+        }
+    }
+
+    private static int getInventorySize(double n){
+        // x = 9
+        if (9 > n) return 9;
+
+        n = n + 4.5;
+        n = n - (n%9);
+        return (int)n;
     }
 }
