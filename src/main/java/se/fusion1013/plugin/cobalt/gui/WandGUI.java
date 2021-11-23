f91784c989c1ca116c922cb6eefccf0564bcf26f
Fusion1013 <william.reutmer@gmail.com>
2021-11-23 15:48:33 +0100

    add spell saving logic

--- src/main/java/se/fusion1013/plugin/cobalt/gui/WandGUI.java
+++ src/main/java/se/fusion1013/plugin/cobalt/gui/WandGUI.java
@@ -3,16 +3,35 @@ package se.fusion1013.plugin.cobalt.gui;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import se.fusion1013.plugin.cobalt.spells.ISpell;
+import se.fusion1013.plugin.cobalt.spells.Spell;
+import se.fusion1013.plugin.cobalt.wand.Wand;
+
+import java.util.List;
 
 public class WandGUI extends AbstractGUI {
 
     static int realInvSize;
+    public Wand wand; // TODO: Add getter
 
-    public WandGUI(int invSize, String invName) {
-        super(getInventorySize(invSize), invName);
+    public WandGUI(Wand wand, String invName) {
+        super(getInventorySize(wand.getCapacity()), invName);
+        int invSize = wand.getCapacity();
         realInvSize = getInventorySize(invSize);
+        this.wand = wand;
 
         setInaccessibleSlots(realInvSize-invSize);
+        addSpells();
+    }
+
+    private void addSpells(){
+        List<ISpell> spells = wand.getSpells();
+
+        for (int i = 0; i < spells.size(); i++) {
+            ISpell spell = spells.get(i);
+            ItemStack item = Spell.getSpellItem(spell.getSpellName());
+            setItem(i, item);
+        }
     }
 
     private void setInaccessibleSlots(int count){
