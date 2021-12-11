c1ae26fc30ad8b1375210e8f1e70f20fa5985e42
Fusion1013 <william.reutmer@gmail.com>
2021-12-11 22:31:05 +0100

    Remove unused getters/setters

--- src/main/java/se/fusion1013/plugin/cobalt/spells/ISpell.java
+++ src/main/java/se/fusion1013/plugin/cobalt/spells/ISpell.java
@@ -1,32 +1,57 @@
 package se.fusion1013.plugin.cobalt.spells;
 
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Vector;
 import se.fusion1013.plugin.cobalt.wand.Wand;
 
+import java.util.List;
+
 public interface ISpell {
-    void castSpell(Wand wand, Player player);
-    void tick();
+    void castSpell(Wand wand, Player caster);
+    void castSpell(Wand wand, Player caster, Vector direction, Location location);
 
-    // Spell attributes
-    int getUses(); // Number of times a spell can be cast before it is depleted
-    int getManaDrain(); // The mana drain of the spell on cast
-    double getDamage(); // The amount of damage dealt by the spell // TODO: Implement different damage types
-    double getRadius(); // The radius of the circular area in which the damage of the spell is dealt
-    double getSpread(); // The range of deviation of a spell from the aimed direction, measured in degrees (Lower number => More accurate)
-    double getSpeed(); // The rate at which the projectile travels
-    double getLifetime(); // The duration of which the projectile remains active
-    double getCastDelay(); // A modifier to the wands base cast delay, determining the delay before the next spell in the wand's queue is cast
-    double getRechargeTime(); // A modifier to the wands base recharge time, triggered after the last spell in the wands queue is cast. The wand reloads all spells in the queue, ready to cast again
-    double getSpreadModifier(); // A modifier to the spread of the wand. A negative spread modifier reduces the positive spread of a wand when applied to the spell
-    double getSpeedModifier(); // A modifier to the speed of the affected projectile. Speed modifiers over 1 will increase projectile speed
-    double getLifetimeModifier(); // A modifier to the lifetime of the affected projectile
-    double getCriticalChance(); // The chance that a spell will critically strike, dealing 5 times the usual damage, and dealing even further damage if critical chance is above 100%
-
-    String getSpellName();
-    boolean getHasCast(); // True if the spell has been cast. Resets when the wand recharges
-    int getAddCasts(); // Increases the number of spells the wand can cast
+    List<String> getLore();
     int getId();
-    String getFormattedName();
+
+    boolean getHasCast();
+    void setHasCast(boolean hasCast);
+    int getManaDrain();
+
+    /**
+     * Gets the mana drain of the spell, including childrens mana drain
+     * @return true mana drain of the spell
+     */
+    int getTrueManaDrain();
+    double getCastDelay();
+
+    /**
+     * Gets the cast delay of the spell, including childrens cast delay
+     * @return true cast delay of the spell
+     */
+    double getTrueCastDelay();
+    double getRechargeTime();
+
+    String getInternalSpellName();
+    String getSpellName();
+    ItemStack getSpellItem();
     int getCustomModelData();
+    Spell.SpellType getSpellType();
+    int getUses();
+    String getDescription();
+    List<Spell.DelayedSpell> getDelayedSpells();
+
+    /**
+     * Performs operations that need to be done before a spell can be cast
+     *
+     * @param wandSpells the spells that are in the wand
+     * @param casts
+     * @param spellPos
+     */
+    void performPreCast(List<ISpell> wandSpells, int casts, int spellPos);
+    void addModifiers(List<IModifier> modifiers);
+
+    Spell clone();
 }
