ddb5afbbd75f6769fa7a3f7944b26851c7e4a7bd
Fusion1013 <william.reutmer@gmail.com>
2021-12-11 22:25:21 +0100

    Switch to CommandAPI registration

--- src/main/java/se/fusion1013/plugin/cobalt/manager/CommandManager.java
+++ src/main/java/se/fusion1013/plugin/cobalt/manager/CommandManager.java
@@ -14,8 +14,18 @@ import java.util.*;
 
 public class CommandManager extends Manager {
 
-    private static CommandManager instance = null;
-    private HashMap<String, CobaltCommand> cobaltCommands = new HashMap<>();
+    private static CommandManager INSTANCE = null;
+    /**
+     * Returns the object representing this <code>CommandManager</code>.
+     *
+     * @return The object of this class
+     */
+    public static CommandManager getInstance(){
+        if (INSTANCE == null){
+            INSTANCE = new CommandManager(Cobalt.getInstance());
+        }
+        return INSTANCE;
+    }
 
     /**
      * Creates a new <code>CommandManager</code> object.
@@ -23,42 +33,6 @@ public class CommandManager extends Manager {
      */
     public CommandManager(Cobalt cobalt) {
         super(cobalt);
-    }
-
-    /**
-     * Returns the object representing this <code>CommandManager</code>.
-     *
-     * @return The object of this class
-     */
-    public static CommandManager getInstance(){
-        if (instance == null){
-            instance = new CommandManager(Cobalt.getInstance());
-        }
-        return instance;
-    }
-
-    /**
-     * Creates and registers a <code>MainCommand</code>.
-     * This will also inject it into Bukkit's central <code>CommandMap</code>.
-     *
-     * @param plugin The plugin that owns the given command.
-     * @param commandClass The class of the command to register.
-     */
-    public <T extends MainCommand> void registerMainCommand(Plugin plugin, Class<T> commandClass){
-        try {
-            // Instantiate and populate the command
-            MainCommand command = commandClass.getDeclaredConstructor().newInstance();
-            command.populate(plugin);
-
-            // Register the command
-            cobaltCommands.put(command.getCommandName(), command);
-            PluginCommand pluginCommand = getCommand(command.getCommandName(), plugin);
-            pluginCommand.setAliases(command.getAliases());
-            getCommandMap().register(plugin.getDescription().getName(), command);
-
-        } catch (Exception ex){
-            ex.printStackTrace();
-        }
     }
 
     private static PluginCommand getCommand(String name, Plugin plugin){
@@ -69,17 +43,7 @@ public class CommandManager extends Manager {
             c.setAccessible(true);
 
             command = c.newInstance(name, plugin);
-        } catch (SecurityException e) {
-            e.printStackTrace();
-        } catch (IllegalArgumentException e){
-            e.printStackTrace();
-        } catch (IllegalAccessException e){
-            e.printStackTrace();
-        } catch (InstantiationException e){
-            e.printStackTrace();
-        } catch (InvocationTargetException e){
-            e.printStackTrace();
-        } catch (NoSuchMethodException e){
+        } catch (SecurityException | IllegalArgumentException | IllegalAccessException | InstantiationException | InvocationTargetException | NoSuchMethodException e) {
             e.printStackTrace();
         }
 
@@ -109,43 +73,8 @@ public class CommandManager extends Manager {
         return commandMap;
     }
 
-    /**
-     * Creates and registers a <code>SubCommand</code>.
-     *
-     * @param plugin The plugin that owns the given command.
-     * @param commandClass The class of the command to register.
-     */
-    public <T extends SubCommand> void registerSubCommand(Plugin plugin, Class<T> commandClass){
-        try {
-            // Instantiate and populate the command
-            SubCommand command = commandClass.getDeclaredConstructor().newInstance();
-            command.populate(plugin);
-
-            // Register the command
-            String parentCommandName = command.getParentCommandName();
-            getCommandFromName(parentCommandName).registerSubCommand(command);
-            String commandKey = parentCommandName + " " + command.getCommandName();
-            cobaltCommands.put(commandKey, command);
-
-        } catch (Exception e){
-            plugin.getLogger().severe("Possibly attempted to register a SubCommand before its parent!");
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Gets the <code>CobaltCommand</code> with the given command name.
-     *
-     * @param commandName The name of the command to get.
-     * @return The <code>CobaltCommand</code>, if found, or null;
-     */
-    public CobaltCommand getCommandFromName(String commandName){
-        return cobaltCommands.get(commandName);
-    }
-
     @Override
     public void reload() {
-
     }
 
     @Override
