f00a1f7d0d7e88e46d28076dd19a8dd911ddd19d
Fusion1013 <35802522+Fusion1013@users.noreply.github.com>
2021-12-13 23:49:11 +0100

    Add particlesOnSphere & particlesInSphere methods

--- src/main/java/se/fusion1013/plugin/cobalt/particle/styles/ParticleStyleSphere.java
+++ src/main/java/se/fusion1013/plugin/cobalt/particle/styles/ParticleStyleSphere.java
@@ -10,6 +10,7 @@ import java.util.List;
 public class ParticleStyleSphere extends ParticleStyle implements IParticleStyle {
     private int density;
     private double radius;
+    private boolean inSphere;
 
     public ParticleStyleSphere(ParticleStyleSphere target){
         super(target);
@@ -30,6 +31,11 @@ public class ParticleStyleSphere extends ParticleStyle implements IParticleStyle
 
     @Override
     public List<PParticle> getParticles(Location location){
+        if (inSphere) return particlesInSphere(location);
+        else return particlesOnSphere(location);
+    }
+
+    private List<PParticle> particlesOnSphere(Location center){
         List<PParticle> particles = new ArrayList<>();
 
         for (int i = 0; i < this.density; i++){
@@ -40,7 +46,29 @@ public class ParticleStyleSphere extends ParticleStyle implements IParticleStyle
             double dx = this.radius * Math.sin(phi) * Math.cos(theta);
             double dy = this.radius * Math.sin(phi) * Math.sin(theta);
             double dz = this.radius * Math.cos(phi);
-            particles.add(new PParticle(location.clone().add(dx, dy, dz), offset.getX(), offset.getY(), offset.getZ(), speed, count));
+            particles.add(new PParticle(center.clone().add(dx, dy, dz), offset.getX(), offset.getY(), offset.getZ(), speed, count));
+        }
+
+        return particles;
+    }
+
+    private List<PParticle> particlesInSphere(Location center){
+        List<PParticle> particles = new ArrayList<>();
+
+        for (int i = 0; i < this.density; i++){
+            double u = Math.random();
+            double v = Math.random();
+            double theta = u * 2.0 * Math.PI;
+            double phi = Math.acos(2.0 * v - 1.0);
+            double r = Math.cbrt(Math.random());
+            double sinTheta = Math.sin(theta);
+            double cosTheta = Math.cos(theta);
+            double sinPhi = Math.sin(phi);
+            double cosPhi = Math.cos(phi);
+            double x = r * sinPhi * cosTheta * this.radius;
+            double y = r * sinPhi * sinTheta * this.radius;
+            double z = r * cosPhi * this.radius;
+            particles.add(new PParticle(center.clone().add(x, y, z), offset.getX(), offset.getY(), offset.getZ(), speed, count));
         }
 
         return particles;
@@ -49,6 +77,8 @@ public class ParticleStyleSphere extends ParticleStyle implements IParticleStyle
     public void setRadius(double radius) { this.radius = radius; }
 
     public void setDensity(int density) { this.density = density; }
+
+    public void setInSphere(boolean onSphere) { this.inSphere = onSphere; }
 
     @Override
     public String getName() {
@@ -69,11 +99,13 @@ public class ParticleStyleSphere extends ParticleStyle implements IParticleStyle
 
         double radius = 1;
         int density = 1; // TODO: Change default value
+        boolean inSphere;
 
         @Override
         public ParticleStyleSphere build(){
             obj.setRadius(radius);
             obj.setDensity(density);
+            obj.setInSphere(inSphere);
             return super.build();
         }
 
@@ -88,6 +120,11 @@ public class ParticleStyleSphere extends ParticleStyle implements IParticleStyle
 
         public ParticleStyleSphereBuilder setDensity(int density){
             this.density = density;
+            return getThis();
+        }
+
+        public ParticleStyleSphereBuilder setInSphere(){
+            this.inSphere = true;
             return getThis();
         }
     }
