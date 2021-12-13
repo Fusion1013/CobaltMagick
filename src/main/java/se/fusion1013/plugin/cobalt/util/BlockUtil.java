a45401ca9323a5fda88d2657c9c6781dec5a9336
Fusion1013 <35802522+Fusion1013@users.noreply.github.com>
2021-12-14 00:07:55 +0100

    Comment methods

--- src/main/java/se/fusion1013/plugin/cobalt/util/BlockUtil.java
+++ src/main/java/se/fusion1013/plugin/cobalt/util/BlockUtil.java
@@ -11,6 +11,14 @@ import java.util.Random;
 
 public class BlockUtil {
 
+    /**
+     * Generates a sphere of block locations around the given center with a given radius.
+     *
+     * @param centerBlock the block to center the sphere on
+     * @param radius the radius of the sphere
+     * @param hollow whether the sphere should be hollow or not
+     * @return a list of locations representing a sphere
+     */
     private static List<Location> generateSphere(Location centerBlock, int radius, boolean hollow){
         if (centerBlock == null) return new ArrayList<>();
 
@@ -38,26 +46,89 @@ public class BlockUtil {
         return circleBlocks;
     }
 
+    /**
+     * Sets blocks in a sphere around a given center, with a given radius and a given block material.
+     *
+     * @param centerBlock the center of the sphere
+     * @param setBlock the block the sphere is made out of
+     * @param radius the radius of the sphere
+     * @return the number of blocks in the sphere
+     */
     public static int setBlocksInSphere(Location centerBlock, Material setBlock, int radius){
         return setBlocksInSphere(centerBlock, setBlock, radius, false, false, false, false);
     }
 
+    /**
+     * Sets blocks in a sphere around a given center, with a given radius and a given block material.
+     *
+     * @param centerBlock the center of the sphere
+     * @param setBlock the block the sphere is made out of
+     * @param radius the radius of the sphere
+     * @param dropItems if the replacement process should drop blocks as items
+     * @return the number of blocks in the sphere
+     */
     public static int setBlocksInSphere(Location centerBlock, Material setBlock, int radius, boolean dropItems){
         return setBlocksInSphere(centerBlock, setBlock, radius, dropItems, false, false, false);
     }
 
+    /**
+     * Sets blocks in a sphere around a given center, with a given radius and a given block material.
+     *
+     * @param centerBlock the center of the sphere
+     * @param setBlock the block the sphere is made out of
+     * @param radius the radius of the sphere
+     * @param dropItems if the replacement process should drop blocks as items
+     * @param slowReplace if the replacement process should be slowed down by a few ticks, randomized for each block in the sphere
+     * @return the number of blocks in the sphere
+     */
     public static int setBlocksInSphere(Location centerBlock, Material setBlock, int radius, boolean dropItems, boolean slowReplace){
         return setBlocksInSphere(centerBlock, setBlock, radius, dropItems, slowReplace, false, false);
     }
 
+    /**
+     * Sets blocks in a sphere around a given center, with a given radius and a given block material.
+     *
+     * @param centerBlock the center of the sphere
+     * @param setBlock the block the sphere is made out of
+     * @param radius the radius of the sphere
+     * @param dropItems if the replacement process should drop blocks as items
+     * @param slowReplace if the replacement process should be slowed down by a few ticks, randomized for each block in the sphere
+     * @param replaceNonAir if non-air blocks should be replaced
+     * @return the number of blocks in the sphere
+     */
     public static int setBlocksInSphere(Location centerBlock, Material setBlock, int radius, boolean dropItems, boolean slowReplace, boolean replaceNonAir){
         return setBlocksInSphere(centerBlock, setBlock, radius, dropItems, slowReplace, replaceNonAir, false);
     }
 
+    /**
+     * Sets blocks in a sphere around a given center, with a given radius and a given block material.
+     *
+     * @param centerBlock the center of the sphere
+     * @param setBlock the block the sphere is made out of
+     * @param radius the radius of the sphere
+     * @param dropItems if the replacement process should drop blocks as items
+     * @param slowReplace if the replacement process should be slowed down by a few ticks, randomized for each block in the sphere
+     * @param replaceNonAir if non-air blocks should be replaced
+     * @param hollow if the sphere should be hollow
+     * @return the number of blocks in the sphere
+     */
     public static int setBlocksInSphere(Location centerBlock, Material setBlock, int radius, boolean dropItems, boolean slowReplace, boolean replaceNonAir, boolean hollow){
         return setBlocksInSphere(centerBlock, setBlock, radius, dropItems, slowReplace, replaceNonAir, hollow, false);
     }
 
+    /**
+     * Sets blocks in a sphere around a given center, with a given radius and a given block material.
+     *
+     * @param centerBlock the center of the sphere
+     * @param setBlock the block the sphere is made out of
+     * @param radius the radius of the sphere
+     * @param dropItems if the replacement process should drop blocks as items
+     * @param slowReplace if the replacement process should be slowed down by a few ticks, randomized for each block in the sphere
+     * @param replaceNonAir if non-air blocks should be replaced
+     * @param hollow if the sphere should be hollow
+     * @param noSound if the blocks being broken should produce sounds
+     * @return the number of blocks in the sphere
+     */
     public static int setBlocksInSphere(Location centerBlock, Material setBlock, int radius, boolean dropItems, boolean slowReplace, boolean replaceNonAir, boolean hollow, boolean noSound){
         if (slowReplace) return setBlocksInSphereSlowly(centerBlock, setBlock, radius, dropItems, replaceNonAir, hollow, noSound);
         else {
@@ -69,6 +140,18 @@ public class BlockUtil {
         }
     }
 
+    /**
+     * Sets blocks in a sphere around a given center, with a given radius and a given block material.
+     *
+     * @param centerBlock the center of the sphere
+     * @param setBlock the block the sphere is made out of
+     * @param radius the radius of the sphere
+     * @param dropItems if the replacement process should drop blocks as items
+     * @param replaceNonAir if non-air blocks should be replaced
+     * @param hollow if the sphere should be hollow
+     * @param noSound if the blocks being broken should produce sounds
+     * @return the number of blocks in the sphere
+     */
     public static int setBlocksInSphereSlowly(Location centerBlock, Material setBlock, int radius, boolean dropItems, boolean replaceNonAir, boolean hollow, boolean noSound){
         List<Location> circleBlocks = generateSphere(centerBlock, radius, hollow);
         Random r = new Random();
@@ -83,6 +166,15 @@ public class BlockUtil {
         return circleBlocks.size();
     }
 
+    /**
+     * Replaces a block at the specified location with another one
+     *
+     * @param location the location of the block to replace
+     * @param setBlock the block type to set it to
+     * @param dropItems if the replacement process should drop the block as an item
+     * @param replaceNonAir if non-air blocks should be replaced
+     * @param noSound if the blocks being broken should produce sounds
+     */
     private static void replaceBlock(Location location, Material setBlock, boolean dropItems, boolean replaceNonAir, boolean noSound){
         Block block = location.getBlock();
 
