0f621bcabad491604bfb903afa02c4f7b1441a1c
Fusion1013 <35802522+Fusion1013@users.noreply.github.com>
2021-12-14 15:40:58 +0100

    Add clone method. Extend AbstractSpellModule

--- src/main/java/se/fusion1013/plugin/cobalt/spells/spellmodules/ParticleModule.java
+++ src/main/java/se/fusion1013/plugin/cobalt/spells/spellmodules/ParticleModule.java
@@ -5,10 +5,9 @@ import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Entity;
 import org.bukkit.util.Vector;
-import se.fusion1013.plugin.cobalt.Cobalt;
 import se.fusion1013.plugin.cobalt.particle.ParticleGroup;
 
-public class ParticleModule implements SpellModule {
+public class ParticleModule extends AbstractSpellModule<ParticleModule> implements SpellModule {
 
     ParticleGroup group;
     boolean cancelsCast;
@@ -18,6 +17,12 @@ public class ParticleModule implements SpellModule {
         this.cancelsCast = cancelsCast;
     }
 
+    public ParticleModule(ParticleModule target){
+        super(target);
+        this.group = target.group.clone();
+        this.cancelsCast = target.cancelsCast;
+    }
+
     @Override
     public void executeOnCast(Location location, Vector velocityVector) {
         display(location);
@@ -25,6 +30,7 @@ public class ParticleModule implements SpellModule {
 
     @Override
     public void executeOnTick(Location location, Vector velocityVector) {
+        super.executeOnTick(location, velocityVector);
         display(location);
     }
 
@@ -51,4 +57,11 @@ public class ParticleModule implements SpellModule {
     public boolean cancelsCast() {
         return cancelsCast;
     }
+
+    @Override
+    public ParticleModule clone() {
+        return new ParticleModule(this);
+    }
+
+    protected ParticleModule getThis() { return this; }
 }
