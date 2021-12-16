e86732e5b67685ecb6fc2e32db9d26b5b9958427
Fusion1013 <35802522+Fusion1013@users.noreply.github.com>
2021-12-16 17:44:36 +0100

    Move methods to AbstractWand

--- src/main/java/se/fusion1013/plugin/cobaltmagick/wand/Wand.java
+++ src/main/java/se/fusion1013/plugin/cobaltmagick/wand/Wand.java
@@ -19,87 +19,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class Wand implements Runnable { // TODO: Move things to abstract class and extend it
-
-    // Cache
-    static List<Wand> wandCache = new ArrayList<>();
-
-    /**
-     * Adds a wand to the cache. This should always be done on plugin load for all wands in the database
-     *
-     * @param wand the wand to add to the cache
-     */
-    public static void addWandToCache(Wand wand) { wandCache.add(wand); }
-
-    /**
-     * Gets a specific wand from the cache
-     *
-     * @param id the id of the wand to get
-     * @return a wand or null
-     */
-    public static Wand getWandFromCache(int id) {
-        for (Wand wand : wandCache) {
-            if (wand.getId() == id) return wand;
-        }
-        return null;
-    }
-
-    /**
-     * Gets all wands in the database and adds them to the cache for easy retrieval. Should be done on startup
-     */
-    public static void loadCacheFromDatabase() {
-        List<Wand> wandsToCache = CobaltMagick.getInstance().getRDatabase().getWands();
-        wandCache = new ArrayList<>(wandsToCache);
-    }
-
-    // Shown Properties
-    boolean shuffle;
-    int spellsPerCast;
-
-    double castDelay;
-    double rechargeTime;
-    int manaMax;
-    int manaChargeSpeed;
-    int capacity;
-    double spread;
-    List<ISpell> alwaysCast;
-
-    double currentMana;
-    double castCooldown;
-    double rechargeCooldown;
-
-    // Hidden Properties
-    int id;
-    int wandTier;
-    int wandId; // This should be unique to all wands
-
-    // Wand Key
-
-    static NamespacedKey wandKey = new NamespacedKey(CobaltMagick.getInstance(), "wand_id");
-
-    /**
-     * Returns the wand key
-     *
-     * @return wand key
-     */
-    public static NamespacedKey getWandKey() { return wandKey; }
-
-    // Stored Spells
-    List<ISpell> spells = new ArrayList<>();
+public class Wand extends AbstractWand implements Runnable { // TODO: Move things to abstract class and extend it
 
     public Wand(boolean shuffle, int spellsPerCast, double castDelay, double rechargeTime, int manaMax, int manaChargeSpeed, int capacity, double spread, List<ISpell> alwaysCast, int wandTier){
-        this.shuffle = shuffle;
-        this.spellsPerCast = spellsPerCast;
-        this.castDelay = castDelay;
-        this.rechargeTime = rechargeTime;
-        this.manaMax = manaMax;
-        this.manaChargeSpeed = manaChargeSpeed;
-        this.capacity = capacity;
-        this.spread = spread;
-        this.alwaysCast = alwaysCast;
-        this.wandTier = wandTier;
+        super(shuffle, spellsPerCast, castDelay, rechargeTime, manaMax, manaChargeSpeed, capacity, spread, alwaysCast, wandTier);
 
-        this.currentMana = manaMax;
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(CobaltMagick.getInstance(), this, 0, 1);
+    }
+
+    public Wand(int cost, int level, boolean forceUnshuffle){
+        super(cost, level, forceUnshuffle);
 
         Bukkit.getScheduler().scheduleSyncRepeatingTask(CobaltMagick.getInstance(), this, 0, 1);
     }
@@ -110,15 +39,11 @@ public class Wand implements Runnable { // TODO: Move things to abstract class a
         LocaleManager localeManager = LocaleManager.getInstance();
         switch (result){
             case CAST_DELAY:
-                // localeManager.sendMessage(p, "wand.spell.cast.cast_delay");
-                // p.playSound(p.getLocation(), Sound.BLOCK_FIRE_EXTINGUISH, 1, 1);
                 break;
             case RECHARGE_TIME:
-                // localeManager.sendMessage(p, "wand.spell.cast.recharge_time");
                 p.playSound(p.getLocation(), Sound.BLOCK_FIRE_EXTINGUISH, 1, 1);
                 break;
             case NO_MANA:
-                // localeManager.sendMessage(p, "wand.spell.cast.no_mana");
                 p.playSound(p.getLocation(), Sound.BLOCK_BEACON_DEACTIVATE, 1, 1);
                 break;
             case SUCCESS:
@@ -174,6 +99,9 @@ public class Wand implements Runnable { // TODO: Move things to abstract class a
         return CastResult.SUCCESS;
     }
 
+    /**
+     * Sets the wand into its recharge state
+     */
     private void recharge(){
         for (ISpell spell : spells){
             spell.setHasCast(false);
@@ -232,156 +160,5 @@ public class Wand implements Runnable { // TODO: Move things to abstract class a
 
         String message = ChatColor.RED + "Recharge" + ChatColor.GRAY + ": " + ChatColor.RED + Math.round(recharge*10)/10.0 + ChatColor.GRAY + "s" + "          " + ChatColor.AQUA + "Mana" + ChatColor.GRAY + ": " + ChatColor.AQUA + Math.round(currentMana);
         p.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(message));
-    }
-
-    /**
-     * Result of a wand cast
-     */
-    public enum CastResult{
-        SUCCESS,
-        RECHARGE_TIME,
-        CAST_DELAY,
-        NO_MANA
-    }
-
-    /**
-     * Saves all wands that are currently in the wand cache to the database
-     */
-    public static void saveAllWandData() {
-        Database db = CobaltMagick.getInstance().getRDatabase();
-        db.updateWandSpells(wandCache);
-    }
-
-    // ----- Getters / Setters -----
-
-    /**
-     * Retrieves a wand from the cache from the given ItemStack
-     * @param stack item to parse the wand from
-     * @return a wand from the cache. Null if no wand was found
-     */
-    public static Wand getWand(ItemStack stack) {
-        ItemMeta meta = stack.getItemMeta();
-        if (meta == null) return null;
-        Integer wandId = meta.getPersistentDataContainer().get(wandKey, PersistentDataType.INTEGER);
-        if (wandId != null) {
-            return getWandFromCache(wandId);
-        }
-        return null;
-    }
-
-    public ItemStack getWandItem(){
-        ItemStack is = new ItemStack(Material.LEATHER_HORSE_ARMOR, 1);
-        ItemMeta meta = is.getItemMeta();
-        if (meta == null) return is;
-
-        NamespacedKey namespacedKey = new NamespacedKey(CobaltMagick.getInstance(), "wand_id");
-        meta.getPersistentDataContainer().set(namespacedKey, PersistentDataType.INTEGER, id);
-
-        meta.setDisplayName(ChatColor.WHITE + "" + ChatColor.BOLD + "Wand");
-        meta.setLore(getLore());
-
-        meta.setCustomModelData(getWandModelData());
-
-        if (meta instanceof  LeatherArmorMeta){
-            LeatherArmorMeta leatherArmorMeta = (LeatherArmorMeta)meta;
-            Random r = new Random();
-            leatherArmorMeta.setColor(Color.fromRGB(r.nextInt(0, 0xffffff)));
-        }
-
-        is.setItemMeta(meta);
-        return is;
-    }
-
-    /**
-     * Gets the wand model depending on the stats of the wand
-     *
-     * @return model id for this wand
-     */
-    public int getWandModelData(){
-        int data = 111;
-
-        if (shuffle) data += 100;
-
-        if (capacity > 14) data += 20;
-        else if (capacity > 4) data += 10;
-
-        if (spellsPerCast >= 3) data += 2;
-        else if (spellsPerCast >= 2) data += 1;
-
-        return data;
-    }
-
-    public List<String> getLore(){
-        List<String> lore = new ArrayList<>();
-
-        if (shuffle) lore.add(ChatColor.WHITE + "Shuffle: " + ChatColor.BLUE + "Yes");
-        else lore.add(ChatColor.WHITE + "Shuffle: " + ChatColor.BLUE + "No");
-        lore.add(ChatColor.WHITE + "Spells/Cast: " + ChatColor.BLUE + spellsPerCast);
-        lore.add(ChatColor.WHITE + "Cast Delay: " + ChatColor.BLUE + castDelay);
-        lore.add(ChatColor.WHITE + "Recharge Time: " + ChatColor.BLUE + rechargeTime);
-        lore.add(ChatColor.WHITE + "Mana Max: " + ChatColor.BLUE + manaMax);
-        lore.add(ChatColor.WHITE + "Mana Charge Speed: " + ChatColor.BLUE + manaChargeSpeed);
-        lore.add(ChatColor.WHITE + "Capacity: " + ChatColor.BLUE + capacity);
-        lore.add(ChatColor.WHITE + "Spread: " + ChatColor.BLUE + spread);
-
-        lore.add(ChatColor.GRAY + "id#" + id);
-        // TODO: Add always casts
-
-        // TODO: Find a way to display the spells that are currently in the wand (Could probably use bundles in the future)
-
-        return lore;
-    }
-
-    public void setSpells(List<ISpell> spells){
-        this.spells = spells;
-    }
-    public List<ISpell> getSpells() { return this.spells; }
-
-    public void setId(int id) { this.id = id; }
-    public int getId() { return id; }
-
-    public boolean isShuffle() {
-        return shuffle;
-    }
-
-    public int getSpellsPerCast() {
-        return spellsPerCast;
-    }
-
-    public double getCastDelay() {
-        return castDelay;
-    }
-
-    public double getRechargeTime() {
-        return rechargeTime;
-    }
-
-    public int getManaMax() {
-        return manaMax;
-    }
-
-    public int getManaChargeSpeed() {
-        return manaChargeSpeed;
-    }
-
-    public int getCapacity() {
-        return capacity;
-    }
-
-    public double getSpread() {
-        return spread;
-    }
-
-    public void setAlwaysCast(List<ISpell> spells) { this.alwaysCast = spells; }
-    public List<ISpell> getAlwaysCast() {
-        return alwaysCast;
-    }
-
-    public int getWandTier() {
-        return wandTier;
-    }
-
-    public int getWandId() {
-        return wandId;
     }
 }
