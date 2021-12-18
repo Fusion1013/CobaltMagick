c345c6949d9512135ab7871f689883cd144f9f64
Fusion1013 <35802522+Fusion1013@users.noreply.github.com>
2021-12-18 20:36:12 +0100

    Update method variables to get wand, caster and spell

--- src/main/java/se/fusion1013/plugin/cobaltmagick/spells/spellmodules/EntitySpellModule.java
+++ src/main/java/se/fusion1013/plugin/cobaltmagick/spells/spellmodules/EntitySpellModule.java
@@ -8,7 +8,10 @@ import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
+import se.fusion1013.plugin.cobaltmagick.spells.ISpell;
+import se.fusion1013.plugin.cobaltmagick.spells.MovableSpell;
 import se.fusion1013.plugin.cobaltmagick.util.GeometryUtil;
+import se.fusion1013.plugin.cobaltmagick.wand.Wand;
 
 public class EntitySpellModule extends AbstractSpellModule<EntitySpellModule> implements SpellModule {
 
@@ -32,42 +35,42 @@ public class EntitySpellModule extends AbstractSpellModule<EntitySpellModule> im
     }
 
     public EntitySpellModule setSummonInSphere(double radius){
-        setRadius(radius);
+        overrideRadius(radius);
         this.inSphere = true;
         return getThis();
     }
 
     @Override
-    public void executeOnTick(Player caster, Location location, Vector velocityVector) {
+    public void executeOnTick(Wand wand, Player caster, ISpell spell) {
         if (!canRun) return;
 
-        summon(location);
+        summon(spell.getLocation());
     }
 
     @Override
-    public void executeOnCast(Player caster, Location location, Vector velocityVector) {
-        summon(location);
+    public void executeOnCast(Wand wand, Player caster, ISpell spell) {
+        summon(spell.getLocation());
     }
 
     @Override
-    public void executeOnBlockHit(Player caster, Location location, Vector velocityVector, Block blockHit, BlockFace hitBlockFace) {
-        super.executeOnBlockHit(caster, location, velocityVector, blockHit, hitBlockFace);
+    public void executeOnBlockHit(Wand wand, Player caster, MovableSpell spell, Block blockHit, BlockFace hitBlockFace) {
+        super.executeOnBlockHit(wand, caster, spell, blockHit, hitBlockFace);
         if (!canRun) return;
 
-        summon(location);
+        summon(spell.getLocation());
     }
 
     @Override
-    public void executeOnEntityHit(Player caster, Location location, Vector velocityVector, Entity entityHit) {
-        super.executeOnEntityHit(caster, location, velocityVector, entityHit);
+    public void executeOnEntityHit(Wand wand, Player caster, MovableSpell spell, Entity entityHit) {
+        super.executeOnEntityHit(wand, caster, spell, entityHit);
         if (!canRun) return;
 
-        summon(location);
+        summon(spell.getLocation());
     }
 
     @Override
-    public void executeOnDeath(Player caster, Location location, Vector velocityVector) {
-        summon(location);
+    public void executeOnDeath(Wand wand, Player caster, ISpell spell) {
+        summon(spell.getLocation());
     }
 
     private void summon(Location location){
