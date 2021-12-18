e249ff6b11041b9ec0b158c5fd228e2eeb15062e
Fusion1013 <35802522+Fusion1013@users.noreply.github.com>
2021-12-18 20:36:49 +0100

    Update method variables to get wand, caster and spell

--- src/main/java/se/fusion1013/plugin/cobaltmagick/spells/spellmodules/SpellModule.java
+++ src/main/java/se/fusion1013/plugin/cobaltmagick/spells/spellmodules/SpellModule.java
@@ -6,13 +6,16 @@ import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
+import se.fusion1013.plugin.cobaltmagick.spells.ISpell;
+import se.fusion1013.plugin.cobaltmagick.spells.MovableSpell;
+import se.fusion1013.plugin.cobaltmagick.wand.Wand;
 
 public interface SpellModule {
-    void executeOnCast(Player caster, Location location, Vector velocityVector);
-    void executeOnTick(Player caster, Location location, Vector velocityVector);
-    void executeOnBlockHit(Player caster, Location location, Vector velocityVector, Block blockHit, BlockFace hitBlockFace);
-    void executeOnEntityHit(Player caster, Location location, Vector velocityVector, Entity entityHit);
-    void executeOnDeath(Player caster, Location location, Vector velocityVector);
+    void executeOnCast(Wand wand, Player caster, ISpell spell);
+    void executeOnTick(Wand wand, Player caster, ISpell spell);
+    void executeOnBlockHit(Wand wand, Player caster, MovableSpell spell, Block blockHit, BlockFace hitBlockFace);
+    void executeOnEntityHit(Wand wand, Player caster, MovableSpell spell, Entity entityHit);
+    void executeOnDeath(Wand wand, Player caster, ISpell spell);
 
     void update();
     boolean cancelsCast();
