24e7a37cb646904d40befd80499bdc116033f86e
Fusion1013 <35802522+Fusion1013@users.noreply.github.com>
2022-01-16 04:01:09 +0100

    Add getters

--- src/main/java/se/fusion1013/plugin/cobaltmagick/item/MagickItem.java
+++ src/main/java/se/fusion1013/plugin/cobaltmagick/item/MagickItem.java
@@ -1,7 +1,10 @@
 package se.fusion1013.plugin.cobaltmagick.item;
 
+import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
 
 public interface MagickItem {
+    NamespacedKey getNamespacedKey();
+    String getInternalName();
     ItemStack getItemStack();
 }
