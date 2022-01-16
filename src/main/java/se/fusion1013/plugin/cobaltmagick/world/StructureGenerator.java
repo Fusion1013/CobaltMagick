9ba240c4aba3f23853daa18e96588b5d4b0ef514
Fusion1013 <35802522+Fusion1013@users.noreply.github.com>
2022-01-16 04:02:37 +0100

    Create StructureGenerator.java

---
+++ src/main/java/se/fusion1013/plugin/cobaltmagick/world/StructureGenerator.java
@@ -0,0 +1,13 @@
+package se.fusion1013.plugin.cobaltmagick.world;
+
+import org.bukkit.event.Listener;
+import org.bukkit.event.world.ChunkLoadEvent;
+
+public class StructureGenerator implements Listener {
+
+    public void onChunkLoad(ChunkLoadEvent event){
+    }
+
+    public void createMagickStatue(){
+    }
+}
