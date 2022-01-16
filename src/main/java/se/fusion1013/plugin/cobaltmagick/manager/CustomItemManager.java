5bb6e9c7d27e9090f92ae5ddd920123ca699884e
Fusion1013 <35802522+Fusion1013@users.noreply.github.com>
2022-01-16 04:01:43 +0100

    Add items

--- src/main/java/se/fusion1013/plugin/cobaltmagick/manager/CustomItemManager.java
+++ src/main/java/se/fusion1013/plugin/cobaltmagick/manager/CustomItemManager.java
@@ -2,13 +2,12 @@ package se.fusion1013.plugin.cobaltmagick.manager;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
 import se.fusion1013.plugin.cobaltmagick.CobaltMagick;
-import se.fusion1013.plugin.cobaltmagick.spells.ISpell;
-import se.fusion1013.plugin.cobaltmagick.spells.Spell;
+import se.fusion1013.plugin.cobaltmagick.item.CustomItem;
+import se.fusion1013.plugin.cobaltmagick.util.HexUtils;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,6 +17,53 @@ import java.util.Map;
 public class CustomItemManager extends Manager {
 
     private static final Map<String, ItemStack> INBUILT_ITEMS = new HashMap<>();
+
+    // ----- TOOLS -----
+
+    public static final CustomItem CRYSTAL_KEY = register(new CustomItem.CustomItemBuilder("crystal_key", Material.EMERALD, 1)
+            .setCustomName(ChatColor.RESET + "" + ChatColor.GREEN + "Crystal Key")
+            .addLoreLine(ChatColor.WHITE + "The key is voiceless")
+            .setCustomModel(3)
+            .build());
+
+    public static final CustomItem DUNGEON_COIN = register(new CustomItem.CustomItemBuilder("dungeon_coin", Material.EMERALD, 1)
+            .setCustomName(ChatColor.RESET + "" + ChatColor.GOLD + "Dungeon Coin")
+            .setCustomModel(1)
+            .build());
+
+    public static final CustomItem DUNGEON_KEY = register(new CustomItem.CustomItemBuilder("dungeon_key", Material.EMERALD, 1)
+            .setCustomName(ChatColor.RESET + "" + ChatColor.GOLD + "Dungeon Key")
+            .setCustomModel(2)
+            .build());
+
+    public static final CustomItem DREAMGLASS = register(new CustomItem.CustomItemBuilder("dreamglass", Material.SPYGLASS, 1)
+            .setCustomName(ChatColor.RESET + "" + ChatColor.DARK_AQUA + "Dreamglass")
+            .addLoreLine(ChatColor.WHITE + "The lens distorts reality")
+            .setCustomModel(1)
+            .build());
+
+    // ----- MATERIALS -----
+
+    public static final CustomItem CRYSTAL_LENS = register(new CustomItem.CustomItemBuilder("crystal_lens", Material.EMERALD, 1)
+            .setCustomName(HexUtils.colorify("&5Crystal Lens")).addLoreLine(HexUtils.colorify("&dLight shifts and distorts")).setCustomModel(4).build());
+
+    public static final CustomItem DRAGONSTONE = register(new CustomItem.CustomItemBuilder("dragonstone", Material.EMERALD, 1)
+            .setCustomName(HexUtils.colorify("&dDragonstone")).setCustomModel(5).build());
+
+    public static final CustomItem MANA_DIAMOND = register(new CustomItem.CustomItemBuilder("mana_diamond", Material.EMERALD, 1)
+            .setCustomName(HexUtils.colorify("&fMana Diamond")).setCustomModel(6).build());
+
+    public static final CustomItem MANA_PEARL = register(new CustomItem.CustomItemBuilder("mana_pearl", Material.EMERALD, 1)
+            .setCustomName(HexUtils.colorify("&fMana Pearl")).setCustomModel(7).build());
+
+    public static final CustomItem MANA_POWDER = register(new CustomItem.CustomItemBuilder("mana_powder", Material.EMERALD, 1)
+            .setCustomName(HexUtils.colorify("&fMana Powder")).setCustomModel(8).build());
+
+    public static final CustomItem RAINBOW_ROD = register(new CustomItem.CustomItemBuilder("rainbow_rod", Material.EMERALD, 1)
+            .setCustomName(HexUtils.colorify("<r:.5:1>Rainbow Rod")).setCustomModel(9).build());
+
+    public static final CustomItem AQUAMARINE = register(new CustomItem.CustomItemBuilder("aquamarine", Material.EMERALD, 1)
+            .setCustomName(HexUtils.colorify("&fAquamarine")).setCustomModel(10).build());
 
     private static CustomItemManager INSTANCE = null;
     /**
@@ -32,41 +78,18 @@ public class CustomItemManager extends Manager {
         return INSTANCE;
     }
 
-    private static ItemStack getItemWithModelData(Material material, int count, int data){
-        ItemStack is = new ItemStack(material, count);
-        ItemMeta meta = is.getItemMeta();
-        if (meta != null) {
-            meta.setCustomModelData(data);
-            is.setItemMeta(meta);
-        }
-        return is;
+    private static CustomItem register(CustomItem item){
+        INBUILT_ITEMS.put(item.getInternalName(), item.getItemStack());
+        return item;
     }
 
-    private void registerItems(){
-
-        // CRYSTAL KEY
-        ItemStack CRYSTAL_KEY = getItemWithModelData(Material.EMERALD, 1, 3);
-        ItemMeta meta = CRYSTAL_KEY.getItemMeta();
-        if (meta != null) {
-            meta.getPersistentDataContainer().set(new NamespacedKey(CobaltMagick.getInstance(), "crystal_key"), PersistentDataType.INTEGER, 1);
-            meta.setDisplayName(ChatColor.RESET + "" + ChatColor.GREEN + "Crystal Key");
-            List<String> keyLore = new ArrayList<>();
-            keyLore.add("The key is voiceless");
-            meta.setLore(keyLore);
-            CRYSTAL_KEY.setItemMeta(meta);
-        }
-        INBUILT_ITEMS.put("crystal_key", CRYSTAL_KEY);
-
-        // DUNGEON COIN
-        ItemStack DUNGEON_COIN = getItemWithModelData(Material.EMERALD, 1, 1);
-        INBUILT_ITEMS.put("dungeon_coin", DUNGEON_COIN);
-
-        // DUNGEON KEY
-        ItemStack DUNGEON_KEY = getItemWithModelData(Material.EMERALD, 1, 2);
-        INBUILT_ITEMS.put("dungeon_key", DUNGEON_KEY);
+    public static boolean isItem(ItemStack item, CustomItem compareTo){
+        ItemMeta meta = item.getItemMeta();
+        if (meta == null) return false;
+        return meta.getPersistentDataContainer().has(compareTo.getNamespacedKey(), PersistentDataType.INTEGER);
     }
 
-    public String[] getItemNames(){
+    public static String[] getItemNames(){
         String[] names = new String[INBUILT_ITEMS.size()];
         List<String> keys = new ArrayList<>(INBUILT_ITEMS.keySet());
         for (int i = 0; i < keys.size(); i++){
@@ -81,7 +104,7 @@ public class CustomItemManager extends Manager {
      * @param name the name of the item to get
      * @return a new item
      */
-    public ItemStack getItem(String name){
+    public static ItemStack getItem(String name){
         return INBUILT_ITEMS.get(name);
     }
 
@@ -91,7 +114,6 @@ public class CustomItemManager extends Manager {
 
     @Override
     public void reload() {
-        registerItems();
     }
 
     @Override
