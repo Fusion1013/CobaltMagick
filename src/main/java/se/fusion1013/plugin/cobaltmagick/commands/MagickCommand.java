4cb4394bb7f5e82ca7d8137cc77d4047319ec9a6
Fusion1013 <35802522+Fusion1013@users.noreply.github.com>
2022-01-18 12:21:38 +0100

    Remove unused code. Add summon and book commands

--- src/main/java/se/fusion1013/plugin/cobaltmagick/commands/MagickCommand.java
+++ src/main/java/se/fusion1013/plugin/cobaltmagick/commands/MagickCommand.java
@@ -5,59 +5,56 @@ import dev.jorel.commandapi.CommandAPI;
 import dev.jorel.commandapi.CommandAPICommand;
 import dev.jorel.commandapi.arguments.*;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BookMeta;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.PluginDescriptionFile;
 import se.fusion1013.plugin.cobaltmagick.CobaltMagick;
 import se.fusion1013.plugin.cobaltmagick.manager.ConfigManager;
 import se.fusion1013.plugin.cobaltmagick.manager.DreamManager;
+import se.fusion1013.plugin.cobaltmagick.manager.EntityManager;
 import se.fusion1013.plugin.cobaltmagick.manager.LocaleManager;
 import se.fusion1013.plugin.cobaltmagick.util.HexUtils;
 import se.fusion1013.plugin.cobaltmagick.util.SchematicUtil;
 import se.fusion1013.plugin.cobaltmagick.util.StringPlaceholders;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.channels.Channels;
-import java.nio.channels.FileChannel;
-import java.nio.channels.ReadableByteChannel;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MagickCommand {
     public static void register(){
 
-        CommandAPICommand versionCommand = createVersionCommand();
-
-        CommandAPICommand colorizeMessage = createColorizeCommand();
-
-        CommandAPICommand colorCommand = createColorCommand();
-
-        CommandAPICommand editCommand = createEditCommand();
-
-        CommandAPICommand dreamCommand = createDreamCommand();
-
-        CommandAPICommand configCommand = createConfigCommand();
-
-        CommandAPICommand loadSchemCommand = new CommandAPICommand("load")
-                .withPermission("cobalt.magick.commands.magick.load")
-                .withArguments(new StringArgument("schematic"))
-                .executesPlayer(MagickCommand::pasteSchem);
-
         // Main magick command
         new CommandAPICommand("magick")
-                .withSubcommand(versionCommand)
-                .withSubcommand(colorizeMessage)
-                .withSubcommand(colorCommand)
-                .withSubcommand(editCommand)
-                .withSubcommand(dreamCommand)
-                .withSubcommand(configCommand)
-                .withSubcommand(loadSchemCommand)
+                .withSubcommand(createVersionCommand())
+                .withSubcommand(createColorizeCommand())
+                .withSubcommand(createColorCommand())
+                .withSubcommand(createEditCommand())
+                .withSubcommand(createDreamCommand())
+                .withSubcommand(createConfigCommand())
+                .withSubcommand(createSummonCommand())
                 .register();
+    }
+
+    private static CommandAPICommand createSummonCommand() {
+        List<String> keys = EntityManager.getInstance().getCustomEntityNames();
+        String[] entityKeys = keys.toArray(new String[0]);
+
+        return new CommandAPICommand("summon")
+                .withPermission("commands.magick.summon")
+                .withArguments(new StringArgument("entity").replaceSuggestions(info -> entityKeys))
+                .executesPlayer(((sender, args) -> {
+                    EntityManager.getInstance().spawnCustomEntity((String)args[0], sender.getLocation());
+                }))
+                .executesCommandBlock(((sender, args) -> {
+                    EntityManager.getInstance().spawnCustomEntity((String)args[0], sender.getBlock().getLocation());
+                }))
+                .executesEntity(((sender, args) -> {
+                    EntityManager.getInstance().spawnCustomEntity((String)args[0], sender.getLocation());
+                }));
     }
 
     private static CommandAPICommand createVersionCommand() {
@@ -100,6 +97,11 @@ public class MagickCommand {
                 });
     }
 
+    /**
+     * Command for editing things
+     *
+     * @return <code>CommandAPICommand</code> command
+     */
     private static CommandAPICommand createEditCommand(){
         return new CommandAPICommand("edit")
                 .withPermission("cobalt.magick.commands.magick.edit")
@@ -111,7 +113,88 @@ public class MagickCommand {
                         .withSubcommand(new CommandAPICommand("custom-name")
                                 .withArguments(new GreedyStringArgument("name"))
                                 .withHelp(HexUtils.colorify("Adds a custom name to an item"), HexUtils.colorify("Adds a custom name to an item. Can be used with color codes. Use &3/magick colors &ffor more info on color codes"))
-                                .executesPlayer(MagickCommand::editItemCustomName)));
+                                .executesPlayer(MagickCommand::editItemCustomName))
+                        .withSubcommand(new CommandAPICommand("book-author")
+                                .withArguments(new StringArgument("author"))
+                                .executesPlayer(MagickCommand::editBookAuthor))
+                        .withSubcommand(new CommandAPICommand("book-generation")
+                                .withArguments(new StringArgument("generation").replaceSuggestions(info -> bookGenerationTypes()))
+                                .executesPlayer(MagickCommand::editBookGeneration))
+                        .withSubcommand(new CommandAPICommand("book-text")
+                                .withArguments(new IntegerArgument("page-index"))
+                                .withArguments(new GreedyStringArgument("text"))
+                                .executesPlayer(MagickCommand::editBookText))
+                        .withSubcommand(new CommandAPICommand("toggle-writable-book")
+                                .executesPlayer(MagickCommand::convertBook)));
+    }
+
+    /**
+     * Converts a book between a Book and Quill and a written book, depending on what the player is currently holding
+     *
+     * @param p player
+     * @param args arguments
+     */
+    private static void convertBook(Player p, Object[] args) {
+        ItemStack stack = p.getInventory().getItemInMainHand();
+        if (stack.getType() == Material.WRITABLE_BOOK) stack.setType(Material.WRITTEN_BOOK);
+        else if (stack.getType() == Material.WRITTEN_BOOK) stack.setType(Material.WRITABLE_BOOK);
+    }
+
+    /**
+     * Edits a book in the players hand
+     *
+     * @param p player that is holding the item
+     * @param author new author of the book
+     * @param generation new generation of the book
+     * @param text new text of the book. Uses HexUtils to color the text
+     * @param page the page to insert the text into
+     */
+    private static void editBook(Player p, String author, String generation, String text, int page) {
+        ItemStack stack = p.getInventory().getItemInMainHand();
+        ItemMeta meta = stack.getItemMeta();
+
+        if (meta == null) return;
+        if (meta instanceof BookMeta bookMeta) {
+            if (author != null) bookMeta.setAuthor(author);
+            if (generation != null) bookMeta.setGeneration(BookMeta.Generation.valueOf(generation.toUpperCase()));
+            if (text != null) {
+                int currentPageCount = bookMeta.getPageCount();
+                if (currentPageCount < page) {
+                    for (int i = currentPageCount; i < page; i++) {
+                        bookMeta.addPage("");
+                    }
+                }
+
+                bookMeta.setPage(page, HexUtils.colorify(text));
+            }
+
+            stack.setItemMeta(bookMeta);
+        }
+    }
+
+    private static void editBookText(Player p, Object[] args) {
+        editBook(p, null, null, (String)args[1], (Integer)args[0]);
+    }
+
+    private static void editBookGeneration(Player p, Object[] args) {
+        editBook(p, null, (String)args[0], null, 0);
+    }
+
+    private static String[] bookGenerationTypes() {
+        BookMeta.Generation[] generations = BookMeta.Generation.values();
+        String[] genStrings = new String[generations.length];
+        for (int i = 0; i < generations.length; i++) genStrings[i] = generations[i].toString().toLowerCase();
+        return genStrings;
+    }
+
+    /**
+     * Sets the author of a book
+     *
+     * @param p player that is holding the book
+     * @param args author of the book
+     */
+    private static void editBookAuthor(Player p, Object[] args) {
+        editBook(p, (String)args[0], null, null, 0);
     }
 
     private static void editItemCustomName(Player p, Object[] args){
