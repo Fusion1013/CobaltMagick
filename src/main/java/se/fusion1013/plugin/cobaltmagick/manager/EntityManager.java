876c986b923a38c0b76765db45501c35bdb2d217
Fusion1013 <35802522+Fusion1013@users.noreply.github.com>
2022-01-18 12:22:38 +0100

    Create EntityManager.java

---
+++ src/main/java/se/fusion1013/plugin/cobaltmagick/manager/EntityManager.java
@@ -0,0 +1,134 @@
+package se.fusion1013.plugin.cobaltmagick.manager;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.boss.BarColor;
+import org.bukkit.boss.BarStyle;
+import org.bukkit.entity.EntityType;
+import se.fusion1013.plugin.cobaltmagick.CobaltMagick;
+import se.fusion1013.plugin.cobaltmagick.entity.AlchemistEntity;
+import se.fusion1013.plugin.cobaltmagick.entity.ICustomEntity;
+import se.fusion1013.plugin.cobaltmagick.entity.WandEntity;
+import se.fusion1013.plugin.cobaltmagick.spells.ISpell;
+import se.fusion1013.plugin.cobaltmagick.wand.Wand;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class EntityManager extends Manager implements Runnable {
+
+    private static EntityManager INSTANCE = null;
+    /**
+     * Returns the object representing this <code>EntityManager</code>.
+     *
+     * @return The object of this class
+     */
+    public static EntityManager getInstance(){
+        if (INSTANCE == null){
+            INSTANCE = new EntityManager(CobaltMagick.getInstance());
+        }
+        return INSTANCE;
+    }
+
+    private static List<ICustomEntity> inbuiltCustomEntities = new ArrayList<>();
+
+    private List<ICustomEntity> summonedCustomEntities;
+
+    public EntityManager(CobaltMagick cobaltMagick) {
+        super(cobaltMagick);
+        INSTANCE = this;
+    }
+
+    private static void registerCustomEntities() {
+        inbuiltCustomEntities = new ArrayList<>();
+
+        // ----- Wand Entity -----
+
+        Wand wandEntityWand = new Wand(false, 1, 0, 4, 1000, 1000, 9, 0, new ArrayList<>(), 0);
+        List<ISpell> wandEntitySpells = new ArrayList<>();
+        wandEntitySpells.add(SpellManager.getSpell(10)); // Set default spell for wand entity
+        wandEntityWand.setSpells(wandEntitySpells);
+        register(new WandEntity(wandEntityWand));
+
+        // ----- Alchemist Entity -----
+
+        register(new AlchemistEntity.AlchemistBuilder()
+                .setMaxHealth(300)
+                .addBossbar("High Alchemist", 50, BarColor.BLUE, BarStyle.SOLID)
+                .addAlwaysDropItem(CustomItemManager.CRYSTAL_KEY.getItemStack())
+                .addXpDrop(6000, 10)
+                .scaleHealth(1.7)
+                .build());
+    }
+
+    private static ICustomEntity register(ICustomEntity entity) {
+        inbuiltCustomEntities.add(entity);
+        return entity;
+    }
+
+    /**
+     * Returns a list of all custom entity names
+     *
+     * @return
+     */
+    public List<String> getCustomEntityNames() {
+        List<String> names = new ArrayList<>();
+        for (ICustomEntity entity : inbuiltCustomEntities) {
+            names.add(entity.getInbuiltName());
+        }
+        return names;
+    }
+
+    public void spawnCustomEntity(ICustomEntity entity, Location location) {
+        if (entity != null) {
+            entity.spawn(location);
+            summonedCustomEntities.add(entity);
+        }
+    }
+
+    public void spawnCustomEntity(String entity, Location location) {
+        ICustomEntity customEntity = getCustomEntity(entity);
+        if (customEntity != null) {
+            ICustomEntity newEntity = customEntity.clone();
+            newEntity.spawn(location);
+            summonedCustomEntities.add(newEntity);
+        }
+    }
+
+    private ICustomEntity getCustomEntity(String entityName) {
+        for (ICustomEntity customEntity : inbuiltCustomEntities) {
+            if (customEntity.getInbuiltName().equalsIgnoreCase(entityName)) return customEntity;
+        }
+        return null;
+    }
+
+    @Override
+    public void reload() {
+        registerCustomEntities();
+        summonedCustomEntities = new ArrayList<>();
+
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(CobaltMagick.getInstance(), this, 0, 1);
+    }
+
+    @Override
+    public void disable() {
+        for (ICustomEntity customEntity : summonedCustomEntities) {
+            customEntity.kill();
+        }
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < summonedCustomEntities.size(); i++) {
+            ICustomEntity customEntity = summonedCustomEntities.get(i);
+
+            customEntity.tick();
+            if (!customEntity.isAlive()) {
+                summonedCustomEntities.remove(i);
+                i--;
+            }
+        }
+    }
+}
