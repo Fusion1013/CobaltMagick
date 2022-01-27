7d480fb57a9853e99f7c6b1d05f648a9b06cfa07
Fusion1013 <william.reutmer@gmail.com>
2022-01-27 19:59:18 +0100

    Create builder

--- src/main/java/se/fusion1013/plugin/cobaltmagick/particle/styles/ParticleStyleLine.java
+++ src/main/java/se/fusion1013/plugin/cobaltmagick/particle/styles/ParticleStyleLine.java
@@ -3,6 +3,7 @@ package se.fusion1013.plugin.cobaltmagick.particle.styles;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.util.Vector;
+import se.fusion1013.plugin.cobaltmagick.CobaltMagick;
 import se.fusion1013.plugin.cobaltmagick.particle.PParticle;
 
 import java.util.ArrayList;
@@ -23,6 +24,7 @@ public class ParticleStyleLine extends ParticleStyle implements IParticleStyle {
 
     public ParticleStyleLine(Particle particle) {
         super("line");
+        this.particle = particle;
         setDefaultSettings();
     }
 
@@ -30,12 +32,12 @@ public class ParticleStyleLine extends ParticleStyle implements IParticleStyle {
     public List<PParticle> getParticles(Location startLocation, Location endLocation) {
         List<PParticle> pParticles = new ArrayList<>();
         double distance = startLocation.distance(endLocation);
-        int steps = density * (int)distance;
-        Vector direction = startLocation.subtract(endLocation).getDirection().normalize();
+        int steps = density * (int)Math.round(distance);
+        Vector direction = startLocation.clone().subtract(endLocation).getDirection().normalize();
 
         for (int i = 0; i < steps; i++){
-            Location location = startLocation.add(direction.multiply((double)i / (double)density));
-            pParticles.add(new PParticle(location));
+            Location location = startLocation.clone().add(direction.clone().multiply((double)i / (double)density));
+            pParticles.add(new PParticle(location, offset.getX(), offset.getY(), offset.getZ(), speed, count));
         }
 
         return pParticles;
@@ -48,5 +50,38 @@ public class ParticleStyleLine extends ParticleStyle implements IParticleStyle {
     @Override
     public ParticleStyle clone() {
         return new ParticleStyleLine(this);
+    }
+
+    public static class ParticleStyleLineBuilder extends ParticleStyleBuilder<ParticleStyleLine, ParticleStyleLineBuilder> {
+
+        int density = 10;
+
+        @Override
+        public ParticleStyleLine build() {
+            obj.setDensity(density);
+
+            return super.build();
+        }
+
+        public ParticleStyleLineBuilder setDensity(int density) {
+            this.density = density;
+            return getThis();
+        }
+
+        @Override
+        protected ParticleStyleLine createObj() {
+            return new ParticleStyleLine(particle);
+        }
+
+        @Override
+        protected ParticleStyleLineBuilder getThis() {
+            return this;
+        }
+    }
+
+    // ----- GETTERS / SETTERS -----
+
+    public void setDensity(int density) {
+        this.density = density;
     }
 }
