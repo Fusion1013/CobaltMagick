bd97ad2aa822b3c61886c91c2bdc6342d16c1c23
Fusion1013 <william.reutmer@gmail.com>
2022-01-27 19:54:42 +0100

    Add getters & setters

--- src/main/java/se/fusion1013/plugin/cobaltmagick/entity/ICustomEntity.java
+++ src/main/java/se/fusion1013/plugin/cobaltmagick/entity/ICustomEntity.java
@@ -1,13 +1,38 @@
 package se.fusion1013.plugin.cobaltmagick.entity;
 
 import org.bukkit.Location;
+import org.bukkit.boss.BarColor;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import se.fusion1013.plugin.cobaltmagick.wand.Wand;
 
 public interface ICustomEntity {
+
+    // ----- EVENTS -----
+
     void spawn(Location location);
     void tick();
-    String getInbuiltName();
+    void onDeath();
+
+    // ----- ENTITY INFORMATION -----
+
+    LivingEntity getEntity();
+
+    double getMaxHealth();
+    double getCurrentHealth();
+
+    // ----- CHECKS -----
+
     boolean isAlive();
+
+    // ----- GETTERS / SETTERS -----
+
+    void switchBossbarColor(BarColor color);
+    String getInbuiltName();
     String getUniqueId();
-    void kill();
+    Location getSpawnLocation();
+
+    // ----- CLONE -----
+
     ICustomEntity clone();
 }
