ab76bf859f3d19502aa4e5dad2d51de1e93981a5
Fusion1013 <william.reutmer@gmail.com>
2022-01-27 19:52:42 +0100

    Move logic to spell manager

--- src/main/java/se/fusion1013/plugin/cobaltmagick/commands/KillSpellsCommand.java
+++ src/main/java/se/fusion1013/plugin/cobaltmagick/commands/KillSpellsCommand.java
@@ -1,27 +1,60 @@
 package se.fusion1013.plugin.cobaltmagick.commands;
 
 import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.arguments.DoubleArgument;
+import dev.jorel.commandapi.executors.CommandBlockCommandExecutor;
 import org.bukkit.entity.Player;
 import se.fusion1013.plugin.cobaltmagick.manager.LocaleManager;
 import se.fusion1013.plugin.cobaltmagick.manager.SpellManager;
 import se.fusion1013.plugin.cobaltmagick.util.StringPlaceholders;
 
+/**
+ * Command for killing all currently active spells
+ */
 public class KillSpellsCommand {
+
+    // ----- REGISTER COMMAND -----
+
+    /**
+     * Registers the killspells commands
+     */
     public static void register(){
-        CommandAPICommand killSpellsCommand = new CommandAPICommand("killspells")
+        new CommandAPICommand("killspells")
                 .withPermission("cobalt.command.killspells")
-                .executesPlayer(KillSpellsCommand::killAllSpells);
-        killSpellsCommand.register();
+                .executesPlayer(KillSpellsCommand::killSpells)
+                .register();
+
+        new CommandAPICommand("killspells")
+                .withPermission("cobalt.command.killspells")
+                .withArguments(new DoubleArgument("max_distance"))
+                .executesPlayer(KillSpellsCommand::killSpells)
+                .executesCommandBlock((CommandBlockCommandExecutor) (sender, args) -> killAllSpells())
+                .register();
     }
 
-    private static void killAllSpells(Player player, Object[] args){
+    // ----- LOGIC -----
+
+    /**
+     * Kills all currently active spells.
+     *
+     * @param player the executor that is killing the spells.
+     * @param args the max distance to the spells. If null kills all spells.
+     */
+    private static void killSpells(Player player, Object[] args){
         LocaleManager localeManager = LocaleManager.getInstance();
-        int nKilled = SpellManager.getInstance().killAllSpells();
+
+        int nKilled;
+        if (args.length > 0) nKilled = SpellManager.getInstance().killAllSpells(player.getLocation(), (int)args[0]);
+        else nKilled = SpellManager.getInstance().killAllSpells();
 
         StringPlaceholders placeholders = StringPlaceholders.builder()
                 .addPlaceholder("killed_spells", nKilled)
                 .build();
 
         localeManager.sendMessage(player, "commands.killspells.killall.success", placeholders);
+    }
+
+    private static void killAllSpells() {
+        SpellManager.getInstance().killAllSpells();
     }
 }
