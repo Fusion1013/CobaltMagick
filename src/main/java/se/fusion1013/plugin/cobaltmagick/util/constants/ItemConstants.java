a17f83f82f911433707f8240ede8f28d5113769e
Fusion1013 <william.reutmer@gmail.com>
2022-06-16 22:17:06 +0200

    Create ItemConstants.java

---
+++ src/main/java/se/fusion1013/plugin/cobaltmagick/util/constants/ItemConstants.java
@@ -0,0 +1,10 @@
+package se.fusion1013.plugin.cobaltmagick.util.constants;
+
+import org.bukkit.NamespacedKey;
+import se.fusion1013.plugin.cobaltmagick.CobaltMagick;
+
+public class ItemConstants {
+
+    public static NamespacedKey SHINY_ORB_USES = new NamespacedKey(CobaltMagick.getInstance(), "shiny_orb_uses");
+
+}
