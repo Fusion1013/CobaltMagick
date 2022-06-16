76c8680ba16aeb14e99b00fb0b934bb58209e33c
Fusion1013 <william.reutmer@gmail.com>
2022-06-16 22:16:42 +0200

    Add warden item drop

--- src/main/java/se/fusion1013/plugin/cobaltmagick/entity/EntityManager.java
+++ src/main/java/se/fusion1013/plugin/cobaltmagick/entity/EntityManager.java
@@ -1,12 +1,18 @@
 package se.fusion1013.plugin.cobaltmagick.entity;
 
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
 import se.fusion1013.plugin.cobaltcore.CobaltCore;
 import se.fusion1013.plugin.cobaltcore.entity.ICustomEntity;
 import se.fusion1013.plugin.cobaltcore.manager.Manager;
+import se.fusion1013.plugin.cobaltmagick.CobaltMagick;
 import se.fusion1013.plugin.cobaltmagick.entity.create.*;
 import se.fusion1013.plugin.cobaltmagick.entity.create.sentientwand.SentientWand;
+import se.fusion1013.plugin.cobaltmagick.item.ItemManager;
 
-public class EntityManager extends Manager {
+public class EntityManager extends Manager implements Listener {
 
     // ----- CONSTRUCTORS -----
 
@@ -25,9 +31,18 @@ public class EntityManager extends Manager {
     public static final ICustomEntity HIGH_ALCHEMIST = HighAlchemist.register();
     public static final ICustomEntity APPRENTICE = Apprentice.register();
 
+    // ----- LISTENERS ----- // TODO: Move into Core
+
+    @EventHandler
+    public void onEntityDeath(EntityDeathEvent event) {
+        if (event.getEntity().getType() == EntityType.WARDEN) {
+            event.getDrops().add(ItemManager.ECHO_INGOT.getItemStack()); // TODO: Move into Cobalt Core
+        }
+    }
 
     @Override
     public void reload() {
+        CobaltMagick.getInstance().getServer().getPluginManager().registerEvents(this, CobaltMagick.getInstance());
     }
 
     @Override
