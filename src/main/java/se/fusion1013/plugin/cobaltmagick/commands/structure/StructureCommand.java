7bb1bf4d590583fd9ab900b0ac3bfe917f999af9
Fusion1013 <william.reutmer@gmail.com>
2022-08-19 14:37:54 +0200

    Register standalone command

--- src/main/java/se/fusion1013/plugin/cobaltmagick/commands/structure/StructureCommand.java
+++ src/main/java/se/fusion1013/plugin/cobaltmagick/commands/structure/StructureCommand.java
@@ -4,12 +4,30 @@ import dev.jorel.commandapi.CommandAPICommand;
 
 public class StructureCommand {
 
-    public static CommandAPICommand createStructureCommand() {
-        return new CommandAPICommand("structure")
+    public static CommandAPICommand register() {
+
+        // Standalone command
+        new CommandAPICommand("cstructure")
+                .withPermission("cobalt.magick.commands.structure")
                 .withSubcommand(DoorCommand.createDoorCommand())
                 .withSubcommand(MusicBoxCommand.createMusicBoxCommand())
                 .withSubcommand(ItemLockCommand.createItemLockCommand())
-                .withSubcommand(RuneLockCommand.createRuneLockCommand());
+                .withSubcommand(RuneLockCommand.createRuneLockCommand())
+                .withSubcommand(LaserCommand.createLaserCommand())
+                .withSubcommand(ActivatableCommand.createActivatableCommand())
+                .withSubcommand(HiddenObjectCommand.createHiddenObjectCommand())
+                .register();
+
+        // Command for /magick
+        return new CommandAPICommand("structure")
+                .withPermission("cobalt.magick.commands.structure")
+                .withSubcommand(DoorCommand.createDoorCommand())
+                .withSubcommand(MusicBoxCommand.createMusicBoxCommand())
+                .withSubcommand(ItemLockCommand.createItemLockCommand())
+                .withSubcommand(RuneLockCommand.createRuneLockCommand())
+                .withSubcommand(LaserCommand.createLaserCommand())
+                .withSubcommand(ActivatableCommand.createActivatableCommand())
+                .withSubcommand(HiddenObjectCommand.createHiddenObjectCommand());
     }
 
 }
