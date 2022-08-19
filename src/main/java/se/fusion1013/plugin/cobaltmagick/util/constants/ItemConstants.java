2af43e7aefb78eba6ef7923c6a178e00431610f8
Fusion1013 <william.reutmer@gmail.com>
2022-08-19 14:50:56 +0200

    Create item constants fro Experience Orb & Death Bound Orb

--- src/main/java/se/fusion1013/plugin/cobaltmagick/util/constants/ItemConstants.java
+++ src/main/java/se/fusion1013/plugin/cobaltmagick/util/constants/ItemConstants.java
@@ -6,5 +6,7 @@ import se.fusion1013.plugin.cobaltmagick.CobaltMagick;
 public class ItemConstants {
 
     public static NamespacedKey SHINY_ORB_USES = new NamespacedKey(CobaltMagick.getInstance(), "shiny_orb_uses");
+    public static NamespacedKey EXPERIENCE_ORB_STORAGE = new NamespacedKey(CobaltMagick.getInstance(), "experience_orb_storage");
+    public static NamespacedKey DEATH_BOUND_ORB_OWNER = new NamespacedKey(CobaltMagick.getInstance(), "death_bound_orb");
 
 }
