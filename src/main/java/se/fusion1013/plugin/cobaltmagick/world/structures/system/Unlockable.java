d0565433cd0d4c5aead73f7fef3a774b6c8161f0
Fusion1013 <william.reutmer@gmail.com>
2022-08-19 14:33:52 +0200

    Delete Unlockable.java

--- src/main/java/se/fusion1013/plugin/cobaltmagick/world/structures/system/Unlockable.java
+++
@@ -1,9 +0,0 @@
-package se.fusion1013.plugin.cobaltmagick.world.structures.system;
-
-import java.util.UUID;
-
-public interface Unlockable {
-    void unlock();
-    boolean isLocked();
-    UUID getUuid();
-}
