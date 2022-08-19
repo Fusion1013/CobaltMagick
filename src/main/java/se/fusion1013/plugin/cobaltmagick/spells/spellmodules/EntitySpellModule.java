4e3782fbce38d88773876e69b3861523771793b6
Fusion1013 <william.reutmer@gmail.com>
2022-08-19 14:49:28 +0200

    Make EntitySpellModule generic

--- src/main/java/se/fusion1013/plugin/cobaltmagick/spells/spellmodules/EntitySpellModule.java
+++ src/main/java/se/fusion1013/plugin/cobaltmagick/spells/spellmodules/EntitySpellModule.java
@@ -8,22 +8,33 @@ import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.util.Consumer;
 import org.bukkit.util.Vector;
 import se.fusion1013.plugin.cobaltmagick.spells.ISpell;
 import se.fusion1013.plugin.cobaltmagick.spells.MovableSpell;
 import se.fusion1013.plugin.cobaltmagick.util.GeometryUtil;
+import se.fusion1013.plugin.cobaltmagick.util.VectorUtils;
 import se.fusion1013.plugin.cobaltmagick.wand.Wand;
 
-public class EntitySpellModule extends AbstractSpellModule<EntitySpellModule> implements SpellModule {
+import java.util.Random;
+
+public class EntitySpellModule<T extends Entity> extends AbstractSpellModule<EntitySpellModule<T>> implements SpellModule {
 
     // ----- VARIABLES -----
 
-    EntityType entity;
+    EntityType entity = null;
+
+    Class<T> entityClass = null;
+    Consumer<T> function = null;
+
     boolean cancelsCast; // TODO: Move cancelsCast to abstract class
 
     // Optional Variables
     boolean inSphere;
     boolean keepSpellVelocity = false;
+
+    double randomVelocitySpreadX = 0;
+    double randomVelocitySpreadY = 0;
 
     // ----- CONSTRUCTORS -----
 
@@ -32,25 +43,44 @@ public class EntitySpellModule extends AbstractSpellModule<EntitySpellModule> im
         this.cancelsCast = cancelsCast;
     }
 
-    public EntitySpellModule(EntitySpellModule target){
+    public EntitySpellModule(Class<T> clazz, Consumer<T> function, boolean cancelsCast) {
+        this.entityClass = clazz;
+        this.function = function;
+        this.cancelsCast = cancelsCast;
+    }
+
+    public EntitySpellModule(EntitySpellModule<T> target){
         super(target);
         this.entity = target.entity;
+
+        this.entityClass = target.entityClass;
+        this.function = target.function;
+
         this.cancelsCast = target.cancelsCast;
 
         this.inSphere = target.inSphere;
         this.keepSpellVelocity = target.keepSpellVelocity;
+
+        this.randomVelocitySpreadX = target.randomVelocitySpreadX;
+        this.randomVelocitySpreadY = target.randomVelocitySpreadY;
     }
 
     // ----- BUILDER METHODS -----
 
-    public EntitySpellModule setSummonInSphere(double radius){
+    public EntitySpellModule<T> setSummonInSphere(double radius){
         overrideRadius(radius);
         this.inSphere = true;
         return getThis();
     }
 
-    public EntitySpellModule setKeepSpellVelocity() {
+    public EntitySpellModule<T> setKeepSpellVelocity() {
         this.keepSpellVelocity = true;
+        return getThis();
+    }
+
+    public EntitySpellModule<T> setRandomVelocitySpread(double x, double y) {
+        this.randomVelocitySpreadX = x;
+        this.randomVelocitySpreadY = y;
         return getThis();
     }
 
@@ -98,15 +128,22 @@ public class EntitySpellModule extends AbstractSpellModule<EntitySpellModule> im
 
         Entity summonedEntity;
 
+        Random r = new Random();
+
         // Modify the spawn location based on optional variables
         if (inSphere) spawnLocation.add(GeometryUtil.getPointInSphere(currentRadius));
 
         // Summon the entity
-        summonedEntity = w.spawnEntity(spawnLocation, entity);
+        if (entity == null) summonedEntity = w.spawn(spawnLocation, entityClass, function);
+        else summonedEntity = w.spawnEntity(spawnLocation, entity);
 
         // Velocity editing methods
         Vector velocity = new Vector();
         if (keepSpellVelocity && spell instanceof MovableSpell movableSpell) velocity.add(movableSpell.getVelocityVector()); // Only do this if the spell is movable
+        Vector rightVector = VectorUtils.getRightVector(velocity);
+        Vector upVector = velocity.clone().rotateAroundAxis(rightVector, Math.PI / 2);
+        if (randomVelocitySpreadX != 0) velocity.rotateAroundAxis(rightVector, Math.toRadians(2 * (r.nextDouble() - .5) * Math.max(0, randomVelocitySpreadX)));
+        if (randomVelocitySpreadY != 0) velocity.rotateAroundAxis(upVector, Math.toRadians(2 * (r.nextDouble() - .5) * Math.max(0, randomVelocitySpreadY)));
         summonedEntity.setVelocity(velocity);
     }
 
@@ -118,12 +155,12 @@ public class EntitySpellModule extends AbstractSpellModule<EntitySpellModule> im
     }
 
     @Override
-    public EntitySpellModule clone() {
-        return new EntitySpellModule(this);
+    public EntitySpellModule<T> clone() {
+        return new EntitySpellModule<T>(this);
     }
 
     @Override
-    protected EntitySpellModule getThis() {
+    protected EntitySpellModule<T> getThis() {
         return this;
     }
 }
