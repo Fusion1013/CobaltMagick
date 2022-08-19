111230c979a34f45921c736a266c8bae49be4f9b
Fusion1013 <william.reutmer@gmail.com>
2022-08-19 14:30:59 +0200

    Create CreateChestStructures.java

---
+++ src/main/java/se/fusion1013/plugin/cobaltmagick/world/structures/register/CreateChestStructures.java
@@ -0,0 +1,81 @@
+package se.fusion1013.plugin.cobaltmagick.world.structures.register;
+
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.util.Vector;
+import se.fusion1013.plugin.cobaltcore.item.loot.CustomLootTable;
+import se.fusion1013.plugin.cobaltcore.item.loot.LootEntry;
+import se.fusion1013.plugin.cobaltcore.item.loot.LootPool;
+import se.fusion1013.plugin.cobaltcore.world.structure.StructureManager;
+import se.fusion1013.plugin.cobaltcore.world.structure.criteria.EnvironmentStructureCriteria;
+import se.fusion1013.plugin.cobaltcore.world.structure.criteria.HeightStructureCriteria;
+import se.fusion1013.plugin.cobaltcore.world.structure.criteria.OnlyInChunkCriteria;
+import se.fusion1013.plugin.cobaltcore.world.structure.modules.LootStructureModule;
+import se.fusion1013.plugin.cobaltcore.world.structure.modules.ReplaceBlocksStructureModule;
+import se.fusion1013.plugin.cobaltcore.world.structure.structure.IStructure;
+import se.fusion1013.plugin.cobaltcore.world.structure.structure.SimpleStructure;
+import se.fusion1013.plugin.cobaltmagick.CobaltMagick;
+import se.fusion1013.plugin.cobaltmagick.item.ItemManager;
+import se.fusion1013.plugin.cobaltmagick.spells.SpellManager;
+import se.fusion1013.plugin.cobaltmagick.util.constants.BookConstants;
+import se.fusion1013.plugin.cobaltmagick.world.structures.modules.MagickChestStructureModule;
+
+public class CreateChestStructures {
+
+    public static void create() {}
+
+    static CustomLootTable light_chest = new CustomLootTable(new CustomLootTable.LootTarget[] {CustomLootTable.LootTarget.CHEST, CustomLootTable.LootTarget.BARREL, CustomLootTable.LootTarget.SHULKER_BOX},
+            new LootPool(1,
+                    new LootEntry(BookConstants.getLightBook(), 1, 1)
+            )
+    );
+
+    static CustomLootTable light_chest_insides = new CustomLootTable(new CustomLootTable.LootTarget[] {CustomLootTable.LootTarget.DROP},
+            new LootPool(4,
+                    new LootEntry(SpellManager.ALPHA.getSpellItem(), 1, 1),
+                    new LootEntry(SpellManager.GAMMA.getSpellItem(), 1, 1),
+                    new LootEntry(SpellManager.MU.getSpellItem(), 1, 1),
+                    new LootEntry(SpellManager.OMEGA.getSpellItem(), 1, 1),
+                    new LootEntry(SpellManager.PHI.getSpellItem(), 1, 1),
+                    new LootEntry(SpellManager.SIGMA.getSpellItem(), 1, 1),
+                    new LootEntry(SpellManager.TAU.getSpellItem(), 1, 1),
+                    new LootEntry(SpellManager.ZETA.getSpellItem(), 1, 1)
+            )
+    );
+
+    static CustomLootTable dark_chest_insides = new CustomLootTable(new CustomLootTable.LootTarget[] {CustomLootTable.LootTarget.DROP},
+            new LootPool(1,
+                    new LootEntry(SpellManager.OCTUPLE_SPELL.getSpellItem(), 1, 1)
+            )
+    );
+
+    public static final IStructure LIGHT_CHEST = StructureManager.registerAlwaysGenerate(new SimpleStructure.SimpleStructureBuilder(CobaltMagick.getInstance(), 19, "light_chest", "structures/light_chest.json")
+            .setNaturalGeneration(true)
+            .setMinDistance(0)
+            .addGenerationCriteria(new OnlyInChunkCriteria(0, 0))
+            .addGenerationCriteria(new HeightStructureCriteria(288, 288))
+            .addStructureModule(new LootStructureModule(light_chest))
+            .addStructureModule(new ReplaceBlocksStructureModule.ReplaceBlocksStructureModuleBuilder().addMaterial(Material.RED_WOOL, Material.STRUCTURE_BLOCK).build())
+            .addStructureModule(new MagickChestStructureModule(Material.STRUCTURE_BLOCK, 2, ItemManager.CRYSTAL_KEY_LIGHT_ACTIVE,
+                    light_chest_insides,
+                    "The chest opens!", "But the key might have other stories to tell..."
+            ))
+            .setGenerationThreshold(-1)
+            .addGenerationCriteria(new EnvironmentStructureCriteria(World.Environment.NORMAL))
+            .build(), new Vector(0, 288, 0));
+
+    public static final IStructure DARK_CHEST = StructureManager.registerAlwaysGenerate(new SimpleStructure.SimpleStructureBuilder(CobaltMagick.getInstance(), 110, "dark_chest", "structures/dark_chest.json")
+            .setNaturalGeneration(true)
+            .setMinDistance(0)
+            .addGenerationCriteria(new OnlyInChunkCriteria(0, 0))
+            .addGenerationCriteria(new HeightStructureCriteria(-64, -64))
+            .addStructureModule(new ReplaceBlocksStructureModule.ReplaceBlocksStructureModuleBuilder().addMaterial(Material.RED_WOOL, Material.STRUCTURE_BLOCK).build())
+            .addStructureModule(new MagickChestStructureModule(Material.STRUCTURE_BLOCK, 2, ItemManager.CRYSTAL_KEY_DARK_ACTIVE,
+                    dark_chest_insides,
+                    "The glass key speaks!", "The chest listens"
+            ))
+            .setGenerationThreshold(-1)
+            .addGenerationCriteria(new EnvironmentStructureCriteria(World.Environment.NORMAL))
+            .build(), new Vector(0, -64, 0));
+
+}
