12527cd6e850d8ccbe541a7c718f82e6029b9b4a
Fusion1013 <william.reutmer@gmail.com>
2022-08-19 14:29:05 +0200

    Create AdvancementCommand.java

---
+++ src/main/java/se/fusion1013/plugin/cobaltmagick/commands/advancement/AdvancementCommand.java
@@ -0,0 +1,82 @@
+package se.fusion1013.plugin.cobaltmagick.commands.advancement;
+
+import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.arguments.ArgumentSuggestions;
+import dev.jorel.commandapi.arguments.StringArgument;
+import org.bukkit.entity.Player;
+import se.fusion1013.plugin.cobaltcore.CobaltCore;
+import se.fusion1013.plugin.cobaltcore.locale.LocaleManager;
+import se.fusion1013.plugin.cobaltcore.util.StringPlaceholders;
+import se.fusion1013.plugin.cobaltmagick.CobaltMagick;
+import se.fusion1013.plugin.cobaltmagick.advancement.MagickAdvancementManager;
+
+public class AdvancementCommand {
+
+    public static CommandAPICommand register() {
+        new CommandAPICommand("cadvancement")
+                .withPermission("cobalt.magick.commands.advancement")
+                .withSubcommand(createAllAdvancementsCommand())
+                .withSubcommand(createGrantAdvancementCommand())
+                .register();
+
+        return new CommandAPICommand("advancement")
+                .withPermission("cobalt.magick.commands.advancement")
+                .withSubcommand(createAllAdvancementsCommand())
+                .withSubcommand(createGrantAdvancementCommand());
+    }
+
+    // ----- ONE ADVANCEMENT -----
+
+    private static CommandAPICommand createGrantAdvancementCommand() {
+        return new CommandAPICommand("grant")
+                .withPermission("cobalt.magick.commands.advancement.grant")
+                .withArguments(new StringArgument("name").replaceSuggestions(ArgumentSuggestions.strings(info -> MagickAdvancementManager.getInstance().getAdvancementNames())))
+                .executesPlayer(AdvancementCommand::grantAdvancement);
+    }
+
+    private static void grantAdvancement(Player player, Object[] args) {
+        MagickAdvancementManager manager = CobaltCore.getInstance().getSafeManager(CobaltMagick.getInstance(), MagickAdvancementManager.class);
+
+        String advancement = (String) args[0];
+
+        // Create locale placeholders
+        StringPlaceholders placeholders = StringPlaceholders.builder()
+                .addPlaceholder("advancement", advancement)
+                .addPlaceholder("player", player.name())
+                .build();
+
+        // If the manager does not exist, return (If this happens something has gone very wrong)
+        if (manager == null) return;
+
+        // Grant advancement and send feedback
+        boolean granted = manager.grantAdvancement(player, advancement);
+        if (granted) LocaleManager.getInstance().sendMessage(CobaltMagick.getInstance(), player, "commands.magick.advancement.grant.success", placeholders);
+        else LocaleManager.getInstance().sendMessage(CobaltMagick.getInstance(), player, "commands.magick.advancement.grant.failed", placeholders);
+    }
+
+    // ----- ALL ADVANCEMENTS -----
+
+    private static CommandAPICommand createAllAdvancementsCommand() {
+        return new CommandAPICommand("all")
+                .withPermission("cobalt.magick.commands.advancement.all")
+                .executesPlayer(AdvancementCommand::grantAllAdvancements);
+    }
+
+    private static void grantAllAdvancements(Player player, Object[] args) {
+        MagickAdvancementManager manager = CobaltCore.getInstance().getSafeManager(CobaltMagick.getInstance(), MagickAdvancementManager.class);
+
+        // Create locale placeholders
+        StringPlaceholders placeholders = StringPlaceholders.builder()
+                .addPlaceholder("player", player.name())
+                .build();
+
+        // If the manager does not exist, return
+        if (manager == null) {
+            LocaleManager.getInstance().sendMessage(CobaltMagick.getInstance(), player, "commands.magick.advancement.grant_all.failed", placeholders);
+            return;
+        }
+        manager.grantAll(player);
+        LocaleManager.getInstance().sendMessage(CobaltMagick.getInstance(), player, "commands.magick.advancement.grant_all.success", placeholders);
+    }
+
+}
