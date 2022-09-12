c767a99895b47550351d1a926eeb8f268e70bc7d
Fusion1013 <william.reutmer@gmail.com>
2022-09-12 23:06:40 +0200

    Add getRevealMethodNames method

--- src/main/java/se/fusion1013/plugin/cobaltmagick/world/structures/hidden/RevealMethod.java
+++ src/main/java/se/fusion1013/plugin/cobaltmagick/world/structures/hidden/RevealMethod.java
@@ -1,6 +1,16 @@
 package se.fusion1013.plugin.cobaltmagick.world.structures.hidden;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum RevealMethod {
     ALL_SEEING_EYE,
-    PROXIMITY
+    PROXIMITY;
+
+    public static String[] getRevealMethodNames() {
+        RevealMethod[] methods = RevealMethod.values();
+        List<String> revealMethodStrings = new ArrayList<>();
+        for (RevealMethod method : methods) revealMethodStrings.add(method.toString());
+        return revealMethodStrings.toArray(new String[0]);
+    }
 }
