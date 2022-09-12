ad219d78cb1809aeffaea641f2f11b4651325ced
Fusion1013 <william.reutmer@gmail.com>
2022-09-12 23:00:08 +0200

    Use performThreadSafeSQLiteOperations method

--- src/main/java/se/fusion1013/plugin/cobaltmagick/database/door/DoorDaoSQLite.java
+++ src/main/java/se/fusion1013/plugin/cobaltmagick/database/door/DoorDaoSQLite.java
@@ -47,15 +47,16 @@ public class DoorDaoSQLite extends Dao implements IDoorDao {
 
     @Override
     public void removeDoorSync(UUID uuid) {
-        try (
-                Connection conn = DataManager.getInstance().getSqliteDb().getSQLConnection();
-                PreparedStatement ps = conn.prepareStatement("DELETE FROM doors WHERE door_id = ?")
-        ) {
-            ps.setString(1, uuid.toString());
-            ps.executeUpdate();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
+        getDataManager().performThreadSafeSQLiteOperations(conn -> {
+            try (
+                    PreparedStatement ps = conn.prepareStatement("DELETE FROM doors WHERE door_id = ?")
+            ) {
+                ps.setString(1, uuid.toString());
+                ps.executeUpdate();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        });
     }
 
     @Override
@@ -67,21 +68,22 @@ public class DoorDaoSQLite extends Dao implements IDoorDao {
 
     @Override
     public void updateDoorStatusSync(MagickDoor[] doors) {
-        try (
-                Connection conn = DataManager.getInstance().getSqliteDb().getSQLConnection();
-                PreparedStatement ps = conn.prepareStatement("UPDATE doors SET is_closed = ? WHERE door_id = ?")
-        ) {
-            conn.setAutoCommit(false);
-            // Loop through all doors and update their status
-            for (MagickDoor door : doors) {
-                ps.setBoolean(1, door.isClosed());
-                ps.setString(2, door.getUuid().toString());
-                ps.executeUpdate();
+        getDataManager().performThreadSafeSQLiteOperations(conn -> {
+            try (
+                    PreparedStatement ps = conn.prepareStatement("UPDATE doors SET is_closed = ? WHERE door_id = ?")
+            ) {
+                conn.setAutoCommit(false);
+                // Loop through all doors and update their status
+                for (MagickDoor door : doors) {
+                    ps.setBoolean(1, door.isClosed());
+                    ps.setString(2, door.getUuid().toString());
+                    ps.executeUpdate();
+                }
+                conn.commit();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
             }
-            conn.commit();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
+        });
     }
 
     @Override
@@ -93,31 +95,32 @@ public class DoorDaoSQLite extends Dao implements IDoorDao {
 
     @Override
     public void insertDoorSync(MagickDoor door) {
-        try (
-                Connection conn = DataManager.getInstance().getSqliteDb().getSQLConnection();
-                PreparedStatement ps = conn.prepareStatement("INSERT INTO doors(door_id, location_uuid, width, height, depth, is_closed) VALUES(?, ?, ?, ?, ?, ?)");
-                PreparedStatement psLocation = conn.prepareStatement("INSERT OR REPLACE INTO locations(uuid, world, x_pos, y_pos, z_pos, yaw, pitch) VALUES(?, ?, ?, ?, ?, ?, ?)")
-        ) {
-            // Insert location
-            psLocation.setString(1, door.getUuid().toString());
-            psLocation.setString(2, door.getCorner().getWorld().getName());
-            psLocation.setDouble(3, door.getCorner().getX());
-            psLocation.setDouble(4, door.getCorner().getY());
-            psLocation.setDouble(5, door.getCorner().getZ());
-            psLocation.setDouble(6, door.getCorner().getYaw());
-            psLocation.setDouble(7, door.getCorner().getPitch());
-            psLocation.execute();
+        getDataManager().performThreadSafeSQLiteOperations(conn -> {
+            try (
+                    PreparedStatement ps = conn.prepareStatement("INSERT INTO doors(door_id, location_uuid, width, height, depth, is_closed) VALUES(?, ?, ?, ?, ?, ?)");
+                    PreparedStatement psLocation = conn.prepareStatement("INSERT OR REPLACE INTO locations(uuid, world, x_pos, y_pos, z_pos, yaw, pitch) VALUES(?, ?, ?, ?, ?, ?, ?)")
+            ) {
+                // Insert location
+                psLocation.setString(1, door.getUuid().toString());
+                psLocation.setString(2, door.getCorner().getWorld().getName());
+                psLocation.setDouble(3, door.getCorner().getX());
+                psLocation.setDouble(4, door.getCorner().getY());
+                psLocation.setDouble(5, door.getCorner().getZ());
+                psLocation.setDouble(6, door.getCorner().getYaw());
+                psLocation.setDouble(7, door.getCorner().getPitch());
+                psLocation.execute();
 
-            ps.setString(1, door.getUuid().toString());
-            ps.setString(2, door.getUuid().toString());
-            ps.setInt(3, door.getWidth());
-            ps.setInt(4, door.getHeight());
-            ps.setInt(5, door.getDepth());
-            ps.setBoolean(6, door.isClosed());
-            ps.executeUpdate();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
+                ps.setString(1, door.getUuid().toString());
+                ps.setString(2, door.getUuid().toString());
+                ps.setInt(3, door.getWidth());
+                ps.setInt(4, door.getHeight());
+                ps.setInt(5, door.getDepth());
+                ps.setBoolean(6, door.isClosed());
+                ps.executeUpdate();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        });
     }
 
     @Override
