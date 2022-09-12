ea0fa8df2902e09f57f41576685de92969129dd3
Fusion1013 <william.reutmer@gmail.com>
2022-09-12 22:51:14 +0200

    Create Rubedo.java

---
+++ src/main/java/se/fusion1013/plugin/cobaltmagick/world/structures/cauldron/scenes/Rubedo.java
@@ -0,0 +1,11 @@
+package se.fusion1013.plugin.cobaltmagick.world.structures.cauldron.scenes;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+public class Rubedo {
+
+    public static void start(Location location, Player amuletOwner) {
+
+    }
+}
