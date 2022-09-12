4b38beab8efb82bb837b05831eea9242abfb8162
Fusion1013 <william.reutmer@gmail.com>
2022-09-12 23:03:45 +0200

    Update book names

--- src/main/java/se/fusion1013/plugin/cobaltmagick/util/constants/BookConstants.java
+++ src/main/java/se/fusion1013/plugin/cobaltmagick/util/constants/BookConstants.java
@@ -1,21 +1,15 @@
 package se.fusion1013.plugin.cobaltmagick.util.constants;
 
-import net.kyori.adventure.inventory.Book;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
-import org.bukkit.inventory.meta.ItemMeta;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 
 public class BookConstants { // TODO: Load from json instead
 
     // ----- MUSIC BOX HINTS -----
 
-    public static ItemStack getLightBook() {
+    public static ItemStack getCunningContraptionBook() {
         ItemStack bookStack = new ItemStack(Material.WRITTEN_BOOK, 1);
         BookMeta meta = (BookMeta) bookStack.getItemMeta();
         meta.setAuthor("???");
@@ -27,13 +21,13 @@ public class BookConstants { // TODO: Load from json instead
         return bookStack;
     }
 
-    public static ItemStack getDarkBook() {
+    public static ItemStack getAlchemistNotebook() {
         ItemStack bookStack = new ItemStack(Material.WRITTEN_BOOK, 1);
         BookMeta meta = (BookMeta) bookStack.getItemMeta();
         meta.setAuthor("???");
         meta.setGeneration(BookMeta.Generation.TATTERED);
         meta.setTitle("Alchemist's Notebook");
-        meta.addPages(Component.text("\"The one below listens to my songs...\n\nE C B G# F\n\nG D# G E A\""));
+        meta.addPages(Component.text("\n\n\"The one below listens to my songs...\n\nE C B G# F\n\nG D# G E A\""));
         meta.setCustomModelData(25);
         bookStack.setItemMeta(meta);
         return bookStack;
