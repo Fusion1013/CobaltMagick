e58d6a7d4801a712ee0d2232adb7cd56ecfec89d
Fusion1013 <william.reutmer@gmail.com>
2022-09-12 23:06:25 +0200

    Integrate with COS

--- src/main/java/se/fusion1013/plugin/cobaltmagick/world/structures/hidden/HiddenObject.java
+++ src/main/java/se/fusion1013/plugin/cobaltmagick/world/structures/hidden/HiddenObject.java
@@ -1,31 +1,38 @@
 package se.fusion1013.plugin.cobaltmagick.world.structures.hidden;
 
+import com.google.gson.JsonObject;
+import dev.jorel.commandapi.arguments.*;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Item;
+import org.bukkit.scheduler.BukkitTask;
 import org.bukkit.util.Vector;
 import se.fusion1013.plugin.cobaltcore.item.CustomItemManager;
 import se.fusion1013.plugin.cobaltcore.particle.ParticleGroup;
-import se.fusion1013.plugin.cobaltcore.world.chunk.IChunkBound;
+import se.fusion1013.plugin.cobaltcore.particle.manager.ParticleGroupManager;
+import se.fusion1013.plugin.cobaltcore.storage.IActivatableStorageObject;
+import se.fusion1013.plugin.cobaltcore.storage.ObjectManager;
+import se.fusion1013.plugin.cobaltcore.util.JsonUtil;
 import se.fusion1013.plugin.cobaltmagick.CobaltMagick;
 import se.fusion1013.plugin.cobaltmagick.wand.Wand;
 import se.fusion1013.plugin.cobaltmagick.wand.WandManager;
 import se.fusion1013.plugin.cobaltmagick.world.WorldManager;
-import se.fusion1013.plugin.cobaltmagick.world.structures.system.IActivatable;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
-public class HiddenObject implements IActivatable, IChunkBound<HiddenObject> {
+public class HiddenObject implements IActivatableStorageObject, Runnable {
 
     // ----- VARIABLES -----
 
     public static final int MAX_REVEAL_DISTANCE = 10;
 
-    private final UUID uuid;
+    private UUID uuid;
 
     // Extra Options
     private boolean hasParticleGroup = false;
-    private ParticleGroup particleGroup = null;
+    private String particleGroup = "";
 
     private boolean spawnsItem = false;
     private String item = "";
@@ -35,11 +42,15 @@ public class HiddenObject implements IActivatable, IChunkBound<HiddenObject> {
 
     private boolean deleteOnActivation = false;
 
-    private final Location location;
-    private final RevealMethod revealMethod;
+    private Location location;
+    private RevealMethod revealMethod;
     private boolean revealed = false;
 
+    private BukkitTask task;
+
     // ----- CONSTRUCTORS -----
+
+    public HiddenObject() {}
 
     public HiddenObject(Location location, RevealMethod revealMethod) {
         this.uuid = UUID.randomUUID();
@@ -83,17 +94,56 @@ public class HiddenObject implements IActivatable, IChunkBound<HiddenObject> {
     public void setParticleGroup(ParticleGroup group) {
         if (group != null) {
             this.hasParticleGroup = true;
-            this.particleGroup = group;
+            this.particleGroup = group.getName();
         } else {
             this.hasParticleGroup = false;
             this.particleGroup = null;
         }
     }
 
+    // ----- ACTIVATING / DISABLING -----
+
+    @Override
+    public void onTrigger(Object... args) {
+        Location triggerLocation = (Location) args[0];
+        RevealMethod triggerMethod = (RevealMethod) args[1];
+
+        if (location.getWorld() != triggerLocation.getWorld()) return;
+
+        if (location.distanceSquared(triggerLocation) <= MAX_REVEAL_DISTANCE * MAX_REVEAL_DISTANCE && triggerMethod == revealMethod) {
+            activate();
+        }
+    }
+
+    @Override
+    public void activate(Object... objects) {
+        revealed = true;
+    }
+
+    @Override
+    public void deactivate(Object... objects) {
+        revealed = false;
+    }
+
+    // ----- LOADING / UNLOADING -----
+
+    @Override
+    public void onLoad() {
+        this.task = Bukkit.getScheduler().runTaskTimerAsynchronously(CobaltMagick.getInstance(), this, 0, 2);
+    }
+
+    @Override
+    public void onUnload() {
+        if (this.task != null) this.task.cancel();
+    }
+
     // ----- LOGIC -----
 
-    public void tick() {
+    @Override
+    public void run() {
         if (revealed) {
+            ParticleGroup particleGroup = ParticleGroupManager.getParticleGroup(this.particleGroup);
+
             if (hasParticleGroup) particleGroup.display(location);
 
             if (spawnsItem) {
@@ -112,13 +162,53 @@ public class HiddenObject implements IActivatable, IChunkBound<HiddenObject> {
                 }, 0);
             }
 
-            if (deleteOnActivation) WorldManager.removeHiddenObject(uuid);
+            if (deleteOnActivation) ObjectManager.removeStorageObject(uuid, getObjectIdentifier(), location.getChunk());
         }
     }
 
+    // ----- JSON INTEGRATION METHODS -----
+
     @Override
-    public void activate() {
-        this.revealed = true;
+    public JsonObject toJson() {
+        JsonObject jo = new JsonObject();
+        jo.addProperty("uuid", uuid.toString());
+
+        jo.addProperty("has_particle_group", hasParticleGroup);
+        jo.addProperty("particle_group", particleGroup);
+
+        jo.addProperty("spawns_item", spawnsItem);
+        if (spawnsItem) jo.addProperty("item", item);
+
+        jo.addProperty("spawns_wand", spawnsWand);
+        jo.addProperty("wand_level", wandLevel);
+
+        jo.addProperty("delete_on_activation", deleteOnActivation);
+
+        jo.add("location", JsonUtil.toJson(location));
+        if (revealMethod != null) jo.addProperty("reveal_method", revealMethod.toString());
+        jo.addProperty("revealed", revealed);
+
+        return jo;
+    }
+
+    @Override
+    public void fromJson(JsonObject jsonObject) {
+        this.uuid = UUID.fromString(jsonObject.get("uuid").getAsString());
+
+        this.hasParticleGroup = jsonObject.get("has_particle_group").getAsBoolean();
+        this.particleGroup = jsonObject.get("particle_group").getAsString();
+
+        this.spawnsItem = jsonObject.get("spawns_item").getAsBoolean();
+        if (spawnsItem) this.item = jsonObject.get("item").getAsString();
+
+        this.spawnsWand = jsonObject.get("spawns_wand").getAsBoolean();
+        this.wandLevel = jsonObject.get("wand_level").getAsInt();
+
+        this.deleteOnActivation = jsonObject.get("delete_on_activation").getAsBoolean();
+
+        this.location = JsonUtil.toLocation(jsonObject.getAsJsonObject("location"));
+        this.revealMethod = RevealMethod.valueOf(jsonObject.get("reveal_method").getAsString());
+        this.revealed = jsonObject.get("revealed").getAsBoolean();
     }
 
     // ----- GETTERS / SETTERS -----
@@ -129,22 +219,78 @@ public class HiddenObject implements IActivatable, IChunkBound<HiddenObject> {
     }
 
     @Override
-    public UUID getUuid() {
+    public UUID getUniqueIdentifier() {
         return uuid;
     }
 
     @Override
-    public HiddenObject getObject() {
-        return this;
+    public void setUniqueIdentifier(UUID uuid) {
+        this.uuid = uuid;
     }
 
     @Override
-    public UUID getUUID() {
-        return uuid;
+    public String getObjectIdentifier() {
+        return "hidden_object";
     }
 
+    @Override
     public Location getLocation() {
         return location;
+    }
+
+    @Override
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    @Override
+    public void fromCommandArguments(Object[] objects) {
+        // TODO
+    }
+
+    @Override
+    public Argument<?>[] getCommandArguments() {
+        return new Argument[] {
+                new StringArgument("particle_group").replaceSuggestions(ArgumentSuggestions.strings(ParticleGroupManager.getParticleGroupNames())),
+                new StringArgument("item_spawn").replaceSuggestions(ArgumentSuggestions.strings(CustomItemManager.getItemNames())),
+                new IntegerArgument("wand_level"),
+                new BooleanArgument("delete_on_activation"),
+                new StringArgument("reveal_method").replaceSuggestions(ArgumentSuggestions.strings(RevealMethod.getRevealMethodNames()))
+        };
+    }
+
+    @Override
+    public void setValue(String key, Object value) {
+        switch (key) {
+            case "particle_group" -> {
+                this.particleGroup = (String) value;
+                this.hasParticleGroup = true;
+            }
+            case "item_spawn" -> {
+                this.item = (String) value;
+                this.spawnsItem = true;
+            }
+            case "wand_level" -> {
+                this.wandLevel = (int) value;
+                this.spawnsWand = true;
+            }
+            case "delete_on_activation" -> this.deleteOnActivation = (boolean) value;
+            case "reveal_method" -> this.revealMethod = RevealMethod.valueOf((String) value);
+        }
+    }
+
+    @Override
+    public List<String> getInfoStrings() {
+        List<String> info = new ArrayList<>();
+        info.add("UUID: " + uuid.toString());
+        if (hasParticleGroup) info.add("Particle Group: " + particleGroup);
+        if (spawnsItem) info.add("Item: " + item);
+        if (spawnsWand) info.add("Wand Level: " + wandLevel);
+        info.add("Delete on Activation: " + deleteOnActivation);
+        info.add("Location: " + location.toVector());
+        info.add("Reveal Method: " + revealMethod.toString());
+        info.add("Revealed: " + revealed);
+        return info;
     }
 
     public RevealMethod getRevealMethod() {
@@ -152,7 +298,7 @@ public class HiddenObject implements IActivatable, IChunkBound<HiddenObject> {
     }
 
     public String getParticleGroupName() {
-        if (particleGroup != null) return particleGroup.getName();
+        if (particleGroup != null) return particleGroup;
         else return "";
     }
 
@@ -182,5 +328,27 @@ public class HiddenObject implements IActivatable, IChunkBound<HiddenObject> {
 
     public boolean isRevealed() {
         return revealed;
+    }
+
+    // ----- CLONE CONSTRUCTOR & METHOD -----
+
+    public HiddenObject(HiddenObject target) {
+        this.uuid = target.uuid;
+        this.hasParticleGroup = target.hasParticleGroup;
+        this.particleGroup = target.particleGroup;
+        this.spawnsItem = target.spawnsItem();
+        this.item = target.item;
+        this.spawnsWand = target.spawnsWand();
+        this.wandLevel = target.wandLevel;
+        this.deleteOnActivation = target.deleteOnActivation;
+        this.location = target.location;
+        this.revealMethod = target.revealMethod;
+        this.revealed = target.revealed;
+        this.task = target.task;
+    }
+
+    @Override
+    public HiddenObject clone() {
+        return new HiddenObject(this);
     }
 }
