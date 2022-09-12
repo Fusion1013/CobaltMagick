72c81985f5344a06899216fdd152dbfc3428290c
Fusion1013 <william.reutmer@gmail.com>
2022-09-12 23:09:32 +0200

    Add default activate / deactivate methods

--- src/main/java/se/fusion1013/plugin/cobaltmagick/world/structures/system/IActivatable.java
+++ src/main/java/se/fusion1013/plugin/cobaltmagick/world/structures/system/IActivatable.java
@@ -13,8 +13,27 @@ public interface IActivatable {
 
     /**
      * Activates the <code>IActivatable</code>.
+     *
+     * @param uuid the <code>UUID</code> of the object that called this method.
+     */
+    default void activate(UUID uuid) {}
+
+    /**
+     * Activates the <code>IActivatable</code>.
      */
     void activate();
+
+    /**
+     * Deactivates the <code>IActivatable</code>.
+     *
+     * @param uuid the <code>UUID</code> of the object that called this method.
+     */
+    default void deactivate(UUID uuid) {}
+
+    /**
+     * Deactivates the <code>IActivatable</code>.
+     */
+    default void deactivate() {}
 
     /**
      * Get the <code>UUID</code> of the <code>IActivatable</code>.
