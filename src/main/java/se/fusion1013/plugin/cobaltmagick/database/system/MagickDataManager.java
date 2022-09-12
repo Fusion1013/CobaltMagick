3999fc2e880c398d4878994976bde2c6567b7881
Fusion1013 <william.reutmer@gmail.com>
2022-09-12 23:01:02 +0200

    Remove unused storage

--- src/main/java/se/fusion1013/plugin/cobaltmagick/database/system/MagickDataManager.java
+++ src/main/java/se/fusion1013/plugin/cobaltmagick/database/system/MagickDataManager.java
@@ -5,16 +5,10 @@ import se.fusion1013.plugin.cobaltcore.database.system.DataManager;
 import se.fusion1013.plugin.cobaltcore.manager.Manager;
 import se.fusion1013.plugin.cobaltmagick.database.door.DoorDaoSQLite;
 import se.fusion1013.plugin.cobaltmagick.database.door.IDoorDao;
-import se.fusion1013.plugin.cobaltmagick.database.hidden.HiddenObjectDaoSQLite;
-import se.fusion1013.plugin.cobaltmagick.database.hidden.IHiddenObjectDao;
-import se.fusion1013.plugin.cobaltmagick.database.itemlock.IItemLockDao;
-import se.fusion1013.plugin.cobaltmagick.database.itemlock.ItemLockDaoSQLite;
 import se.fusion1013.plugin.cobaltmagick.database.laser.ILaserDao;
 import se.fusion1013.plugin.cobaltmagick.database.laser.LaserDaoSQLite;
 import se.fusion1013.plugin.cobaltmagick.database.musicbox.IMusicBoxDao;
 import se.fusion1013.plugin.cobaltmagick.database.musicbox.MusicBoxDaoSQLite;
-import se.fusion1013.plugin.cobaltmagick.database.runelock.IRuneLockDao;
-import se.fusion1013.plugin.cobaltmagick.database.runelock.RuneLockDaoSQLite;
 import se.fusion1013.plugin.cobaltmagick.database.wand.IWandDao;
 import se.fusion1013.plugin.cobaltmagick.database.wand.WandDaoSQLite;
 
@@ -41,12 +35,9 @@ public class MagickDataManager extends Manager {
     private void initDao() {
         // SQLITE
         core.getManager(core, DataManager.class).registerDao(new DoorDaoSQLite(), IDoorDao.class);
-        core.getManager(core, DataManager.class).registerDao(new ItemLockDaoSQLite(), IItemLockDao.class);
         core.getManager(core, DataManager.class).registerDao(new MusicBoxDaoSQLite(), IMusicBoxDao.class);
         core.getManager(core, DataManager.class).registerDao(new WandDaoSQLite(), IWandDao.class);
-        core.getManager(core, DataManager.class).registerDao(new RuneLockDaoSQLite(), IRuneLockDao.class);
         core.getManager(core, DataManager.class).registerDao(new LaserDaoSQLite(), ILaserDao.class);
-        core.getManager(core, DataManager.class).registerDao(new HiddenObjectDaoSQLite(), IHiddenObjectDao.class);
     }
 
     // ----- INSTANCE VARIABLE & METHOD -----
