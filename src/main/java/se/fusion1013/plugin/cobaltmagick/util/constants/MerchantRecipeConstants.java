d5aff88fd48f2ecfc594cffa158a502875b83df7
Fusion1013 <william.reutmer@gmail.com>
2022-11-08 12:34:44 +0100

    Get from item manager

--- src/main/java/se/fusion1013/plugin/cobaltmagick/util/constants/MerchantRecipeConstants.java
+++ src/main/java/se/fusion1013/plugin/cobaltmagick/util/constants/MerchantRecipeConstants.java
@@ -2,6 +2,7 @@ package se.fusion1013.plugin.cobaltmagick.util.constants;
 
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.MerchantRecipe;
+import se.fusion1013.plugin.cobaltcore.item.CustomItemManager;
 import se.fusion1013.plugin.cobaltmagick.item.ItemManager;
 
 import java.util.ArrayList;
@@ -17,49 +18,49 @@ public class MerchantRecipeConstants {
         // YODA
         MerchantRecipe recipe1 = new MerchantRecipe(ItemManager.HAT_YODA.getItemStack(), 1);
         List<ItemStack> recipe1Ingredients = new ArrayList<>();
-        recipe1Ingredients.add(ItemManager.GOLD_COIN.getItemStack());
+        recipe1Ingredients.add(CustomItemManager.getCustomItemStack("gold_coin"));
         recipe1.setIngredients(recipe1Ingredients);
         recipes.add(recipe1);
 
         // MONOCLE
         MerchantRecipe recipe2 = new MerchantRecipe(ItemManager.HAT_MONOCLE.getItemStack(), 1);
         List<ItemStack> recipe2Ingredients = new ArrayList<>();
-        recipe2Ingredients.add(ItemManager.GOLD_COIN.getItemStack());
+        recipe2Ingredients.add(CustomItemManager.getCustomItemStack("gold_coin"));
         recipe2.setIngredients(recipe2Ingredients);
         recipes.add(recipe2);
 
         // DEER HEAD
         MerchantRecipe recipe3 = new MerchantRecipe(ItemManager.HAT_DEER_HEAD.getItemStack(), 1);
         List<ItemStack> recipe3Ingredients = new ArrayList<>();
-        recipe3Ingredients.add(ItemManager.GOLD_COIN.getItemStack());
+        recipe3Ingredients.add(CustomItemManager.getCustomItemStack("gold_coin"));
         recipe3.setIngredients(recipe3Ingredients);
         recipes.add(recipe3);
 
         // CIGAR
         MerchantRecipe recipe4 = new MerchantRecipe(ItemManager.HAT_CIGAR.getItemStack(), 1);
         List<ItemStack> recipe4Ingredients = new ArrayList<>();
-        recipe4Ingredients.add(ItemManager.GOLD_COIN.getItemStack());
+        recipe4Ingredients.add(CustomItemManager.getCustomItemStack("gold_coin"));
         recipe4.setIngredients(recipe4Ingredients);
         recipes.add(recipe4);
 
         // TINY HAT
         MerchantRecipe recipe5 = new MerchantRecipe(ItemManager.HAT_TINY_HAT.getItemStack(), 1);
         List<ItemStack> recipe5Ingredients = new ArrayList<>();
-        recipe5Ingredients.add(ItemManager.GOLD_COIN.getItemStack());
+        recipe5Ingredients.add(CustomItemManager.getCustomItemStack("gold_coin"));
         recipe5.setIngredients(recipe5Ingredients);
         recipes.add(recipe5);
 
         // SPONGEBOB
         MerchantRecipe recipe6 = new MerchantRecipe(ItemManager.HAT_SPONGEBOB.getItemStack(), 1);
         List<ItemStack> recipe6Ingredients = new ArrayList<>();
-        recipe6Ingredients.add(ItemManager.GOLD_COIN.getItemStack());
+        recipe6Ingredients.add(CustomItemManager.getCustomItemStack("gold_coin"));
         recipe6.setIngredients(recipe6Ingredients);
         recipes.add(recipe6);
 
         // BIRD MASTER
         MerchantRecipe recipe7 = new MerchantRecipe(ItemManager.HAT_BIRD_MASTER.getItemStack(), 1);
         List<ItemStack> recipe7Ingredients = new ArrayList<>();
-        recipe7Ingredients.add(ItemManager.GOLD_COIN.getItemStack());
+        recipe7Ingredients.add(CustomItemManager.getCustomItemStack("gold_coin"));
         recipe7.setIngredients(recipe7Ingredients);
         recipes.add(recipe7);
 
